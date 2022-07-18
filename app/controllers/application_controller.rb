dc08db41748de8eae2f7f75cf8d0b0d65ce7baf9
Jesus Laiz <zheileman@users.noreply.github.com>
2022-07-18 14:23:38 +0100

    Add initialisation of the current crime application
    And some step helpers used to build the forms, the back link and show errors.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  include StepsHelper
+
+  def current_crime_application
+    @current_crime_application ||= CrimeApplication.find_by(id: session[:crime_application_id])
+  end
+  helper_method :current_crime_application
+
+  private
+
+  def initialize_crime_application(attributes = {})
+    CrimeApplication.create(attributes).tap do |crime_application|
+      session[:crime_application_id] = crime_application.id
+    end
+  end
 end
