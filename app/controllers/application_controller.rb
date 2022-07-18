cdef00911d7782ca36e4dd509eff102f8d66741f
Jesus Laiz <zheileman@users.noreply.github.com>
2022-07-18 15:05:41 +0100

    Temporary nocov as this is tested via other files
    The nocov annotation will be removed once we have the rest of the files and shared examples pushed.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,20 @@
 class ApplicationController < ActionController::Base
   include StepsHelper
 
+  # :nocov:
   def current_crime_application
     @current_crime_application ||= CrimeApplication.find_by(id: session[:crime_application_id])
   end
   helper_method :current_crime_application
+  # :nocov:
 
   private
 
+  # :nocov:
   def initialize_crime_application(attributes = {})
     CrimeApplication.create(attributes).tap do |crime_application|
       session[:crime_application_id] = crime_application.id
     end
   end
+  # :nocov:
 end
