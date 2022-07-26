75ede059f894d3acdc888ca7fc60d3dbc0409182
Jesus Laiz <zheileman@users.noreply.github.com>
2022-07-26 15:42:29 +0100

    Add date of birth attribute to client details form
    It uses the `:multiparam_date` attribute type, as well as the `multiparam_date` validator.

--- app/forms/steps/client/details_form.rb
+++ app/forms/steps/client/details_form.rb
@@ -8,9 +8,12 @@ module Steps
       attribute :first_name, :string
       attribute :last_name, :string
       attribute :other_names, :string
+      attribute :date_of_birth, :multiparam_date
 
       validates_presence_of :first_name,
                             :last_name
+
+      validates :date_of_birth, presence: true, multiparam_date: true
 
       private
 
