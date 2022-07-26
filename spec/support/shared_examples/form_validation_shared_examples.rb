71d595e9e31aa8fd4c2f400570ab6619ef2681b5
Jesus Laiz <zheileman@users.noreply.github.com>
2022-07-26 12:30:45 +0100

    Implement an extensive custom date validator
    This date validator performs most of the heavy validation on multi param dates (apart from any additional ones that can be added to the standard Rails validator, like `presence`).
    
    It works under the assumption that we can have a "valid" date object, but we might want additional logic validations on this, for example some dates, like DOB can't be in the future.
    
    Or we can have an invalid date not casted to a date object, thus we have the raw multi param values (3 values for day, month and year) and we can perform granular validation on these so we can present a more accurate error than just "date is invalid".
    
    For example we validate `day` is no greater than 31, or month is not greater than 12, etc. and set appropriate errors.
    
    It can also check for blank values (like having entered only day and month, but not year) etc.
    
    To use it, just add to your attribute the validation:
    
    ```ruby
    validates :date_of_birth, presence: true, multiparam_date: true
    ```
    
    If the attribute is not mandatory and can be left blank, remove `presence: true`.
    
    If dates in the future are allowed (by default they are not), use like this:
    
    ```ruby
    validates :date_of_birth, presence: true, multiparam_date: { allow_future: true }
    ```

--- spec/support/shared_examples/form_validation_shared_examples.rb
+++ spec/support/shared_examples/form_validation_shared_examples.rb
@@ -39,3 +39,126 @@ RSpec.shared_examples 'a has-one-association form' do |options|
     end
   end
 end
+
+RSpec.shared_examples 'a multiparam date validation' do |options|
+  let(:attribute_name) { options[:attribute_name] }
+
+  before do
+    subject.public_send("#{attribute_name}=", date)
+  end
+
+  context 'when day is invalid' do
+    let(:date) { { 3 => 32, 2 => 12, 1 => 2020 } }
+
+    it 'has a validation error on the field' do
+      expect(subject).to_not be_valid
+      expect(subject.errors.added?(attribute_name, :invalid_day)).to eq(true)
+    end
+  end
+
+  context 'when day is missing' do
+    let(:date) { { 2 => 12, 1 => 2020 } }
+
+    it 'has a validation error on the field' do
+      expect(subject).to_not be_valid
+      expect(subject.errors.added?(attribute_name, :invalid_day)).to eq(true)
+    end
+  end
+
+  context 'when month is invalid' do
+    let(:date) { { 3 => 25, 2 => 13, 1 => 2020 } }
+
+    it 'has a validation error on the field' do
+      expect(subject).to_not be_valid
+      expect(subject.errors.added?(attribute_name, :invalid_month)).to eq(true)
+    end
+  end
+
+  context 'when month is missing' do
+    let(:date) { { 3 => 25, 1 => 2020 } }
+
+    it 'has a validation error on the field' do
+      expect(subject).to_not be_valid
+      expect(subject.errors.added?(attribute_name, :invalid_month)).to eq(true)
+    end
+  end
+
+  context 'when year is invalid (too old)' do
+    let(:date) { { 3 => 25, 2 => 12, 1 => 1919 } }
+
+    it 'has a validation error on the field' do
+      expect(subject).to_not be_valid
+      expect(subject.errors.added?(attribute_name, :year_too_early)).to eq(true)
+    end
+  end
+
+  context 'when year is invalid (too futuristic)' do
+    let(:date) { { 3 => 25, 2 => 12, 1 => 2051 } }
+
+    it 'has a validation error on the field' do
+      expect(subject).to_not be_valid
+      expect(subject.errors.added?(attribute_name, :year_too_late)).to eq(true)
+    end
+  end
+
+  context 'when year is missing' do
+    let(:date) { { 3 => 25, 2 => 12 } }
+
+    it 'has a validation error on the field' do
+      expect(subject).to_not be_valid
+      expect(subject.errors.added?(attribute_name, :invalid_year)).to eq(true)
+    end
+  end
+
+  context 'when date contains garbage values' do
+    let(:date) { { 3 => 'foo', 2 => 2, 1 => 'bar' } }
+
+    it 'has a validation error on the field' do
+      expect(subject).to_not be_valid
+      expect(subject.errors.added?(attribute_name, :invalid)).to eq(true)
+    end
+  end
+
+  context 'when date is not valid (non-leap year)' do
+    let(:date) { { 3 => 29, 2 => 2, 1 => 2021 } }
+
+    it 'has a validation error on the field' do
+      expect(subject).to_not be_valid
+      expect(subject.errors.added?(attribute_name, :invalid)).to eq(true)
+    end
+  end
+
+  context 'when date is in the future' do
+    let(:date) { Date.tomorrow }
+
+    # `false` is the validator default unless passing a different config
+    if options.fetch(:allow_future, false)
+      it 'allows future dates' do
+        expect(subject).to be_valid
+        expect(subject.errors.added?(attribute_name, :future_not_allowed)).to eq(false)
+      end
+    else
+      it 'does not allow future dates' do
+        expect(subject).to_not be_valid
+        expect(subject.errors.added?(attribute_name, :future_not_allowed)).to eq(true)
+      end
+    end
+  end
+
+  context 'when date is in the past' do
+    let(:date) { Date.yesterday }
+
+    # `true` is the validator default unless passing a different config
+    if options.fetch(:allow_past, true)
+      it 'allows past dates' do
+        expect(subject).to be_valid
+        expect(subject.errors.added?(attribute_name, :past_not_allowed)).to eq(false)
+      end
+    else
+      it 'does not allow past dates' do
+        expect(subject).to_not be_valid
+        expect(subject.errors.added?(attribute_name, :past_not_allowed)).to eq(true)
+      end
+    end
+  end
+end
