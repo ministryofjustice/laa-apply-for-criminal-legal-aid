ef8e37454e1893d8854db3a0cf0eef1333e5fcea
Jesus Laiz <zheileman@users.noreply.github.com>
2022-07-26 12:21:32 +0100

    Register new ActiveModel type for multi param dates
    Similar to what we already did for the yes-no value objects, we are registering a new ActiveModel Type, named `:multiparam_date` to use in form objects that use dates.
    
    This custom type is only necessary because if we use the already existing type (`ActiveModel::Type::Date`) it will blow up with a `MultiparameterAssignmentErrors` exception when trying to cast any invalid date.
    
    Also, if the date is invalid it wouldn't return back to the view whatever the user entered in the date, which is not great from a user experience point of view, it will all be blank.
    
    Thus, with this custom type, if the date is invalid `Date.valid_date?` (in addition to not blow up) we return the multi param hash so we can validate this in a custom validator with more granularity.
    
    If the date is valid we just return the date object and still run further validations in the validator.

--- config/initializers/attribute_types.rb
+++ config/initializers/attribute_types.rb
@@ -2,3 +2,4 @@ Dir[File.expand_path('app/attributes/type') + '/*.rb'].each { |f| require f }
 
 ActiveModel::Type.register(:string, Type::StrippedString)
 ActiveModel::Type.register(:yes_no, Type::YesNoType)
+ActiveModel::Type.register(:multiparam_date, Type::MultiparamDate)
---
+++ spec/attributes/multiparam_date_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+RSpec.describe MultiparamDate do
+  subject { described_class.new }
+
+  let(:coerced_value) { subject.cast(value) }
+
+  describe 'when value is already a date' do
+    let(:value) { Date.yesterday }
+    it { expect(coerced_value).to eq(value) }
+  end
+
+  describe 'when value is `nil`' do
+    let(:value) { nil }
+    it { expect(coerced_value).to be_nil }
+  end
+
+  describe 'when value is a multi parameter hash' do
+    let(:date) { Date.new(2008, 11, 22) }
+
+    context 'and contains all needed parts' do
+      let(:value) { { 3 => date.day, 2 => date.month, 1 => date.year } }
+      it { expect(coerced_value).to eq(date) }
+    end
+
+    context 'the parts do not represent a valid date (invalid month)' do
+      let(:value) { { 3 => date.day, 2 => 13, 1 => date.year } }
+      it { expect(coerced_value).to eq(value) }
+    end
+
+    context 'the parts do not represent a valid date (invalid day)' do
+      let(:value) { { 3 => 32, 2 => date.month, 1 => date.year } }
+      it { expect(coerced_value).to eq(value) }
+    end
+
+    context 'any part is set to zero' do
+      let(:value) { { 3 => date.day, 2 => 0, 1 => date.year } }
+      it { expect(coerced_value).to eq(value) }
+    end
+
+    context 'any part is set to nil' do
+      let(:value) { { 3 => date.day, 2 => date.month, 1 => nil } }
+      it { expect(coerced_value).to eq(value) }
+    end
+
+    context 'any part is missing from the hash' do
+      let(:value) { { 3 => date.day, 2 => date.month } }
+      it { expect(coerced_value).to eq(value) }
+    end
+  end
+
+  describe '#value_from_multiparameter_assignment' do
+    context 'override the superclass method' do
+      it { expect(subject.value_from_multiparameter_assignment('foobar')).to eq('foobar') }
+    end
+  end
+end
