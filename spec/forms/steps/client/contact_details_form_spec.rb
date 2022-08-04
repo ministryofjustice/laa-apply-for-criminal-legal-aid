409b09a553fb31fe587f0e18c3f5f118bff44d0d
Arthur Ashman <arthur.ashman@digital.justice.gov.uk>
2022-08-04 12:33:20 +0100

    Add placeholder tests for ContactDetailsFormSpec

--- spec/forms/steps/client/contact_details_form_spec.rb
+++ spec/forms/steps/client/contact_details_form_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Steps::Client::ContactDetailsForm do
+  let(:arguments) { {
+    crime_application: crime_application
+  } }
 
+  let(:crime_application) { instance_double(CrimeApplication) }
+
+  subject { described_class.new(arguments) }
+
+  describe '#save' do
+    context 'when validations pass' do
+      it_behaves_like 'a has-one-association form',
+                      association_name: :applicant,
+                      expected_attributes: {}
+    end
+  end
 end
