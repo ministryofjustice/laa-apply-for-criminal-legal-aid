7c395124ac4213bca86dd814754f949d757a5bac
willmcb <william.mcbrien@digital.justice.gov.uk>
2022-08-08 12:43:11 +0100

    Implement Nino form redesign

--- app/forms/steps/client/has_nino_form.rb
+++ app/forms/steps/client/has_nino_form.rb
@@ -6,17 +6,11 @@ module Steps
       # taken from Civil Apply
       NINO_REGEXP = /\A[A-CEGHJ-PR-TW-Z]{1}[A-CEGHJ-NPR-TW-Z]{1}[0-9]{6}[A-DFM]{1}\Z/
 
-      attribute :has_nino, :yes_no
       attribute :nino, :string
 
       has_one_association :applicant
 
-      validates_inclusion_of :has_nino, in: :choices
-      validates :nino, format: { with: NINO_REGEXP }, if: -> { has_nino&.yes? }
-
-      def choices
-        YesNoAnswer.values
-      end
+      validates :nino, format: { with: NINO_REGEXP }
 
       private
 
--- spec/services/decisions/client_decision_tree_spec.rb
+++ spec/services/decisions/client_decision_tree_spec.rb
@@ -28,16 +28,11 @@ RSpec.describe Decisions::ClientDecisionTree do
   end
 
   context 'when the step is `has_nino`' do
-    let(:form_object) { double('FormObject', applicant: 'applicant', has_nino: has_nino) }
+    let(:form_object) { double('FormObject', applicant: 'applicant') }
     let(:step_name) { :has_nino }
 
-    context 'and answer is `no`' do
-      let(:has_nino) { YesNoAnswer::NO }
-      it { is_expected.to have_destination(:nino_exit, :show) }
-    end
-
-    context 'and answer is `yes`' do
-      let(:has_nino) { YesNoAnswer::YES }
+    context 'has correct next step' do
+      let(:nino) { 'AA123245A' }
 
       before do
         allow(
