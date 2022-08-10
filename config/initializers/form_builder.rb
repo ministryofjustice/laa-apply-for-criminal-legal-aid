1f757e4399b07ffd5d55c25d33d67fa09cef7b5d
Jesus Laiz <zheileman@users.noreply.github.com>
2022-08-10 14:00:19 +0100

    Sort out form builder defaults and fix markup (#57)
    We had non-standard defaults set for the form builder. These are good defaults when an app is mainly done by many 1 question per page, as it saves having to set again and again the same tag, size, etc.
    
    But in reality this app will not have that many individual questions and where we have them, we can customise accordingly.
    
    Removed the custom defaults and tweaked the existing pages where needed to output proper markup, tags and sizes.
    
    Also added `autocomplete: 'off'` if it was missing.

--- config/initializers/form_builder.rb
+++ config/initializers/form_builder.rb
@@ -1,11 +1,5 @@
 ActionView::Base.default_form_builder = GOVUKDesignSystemFormBuilder::FormBuilder
 
-GOVUKDesignSystemFormBuilder.configure do |config|
-  config.default_legend_tag   = 'h1'
-  config.default_legend_size  = 'xl'
-  config.default_caption_size = 'xl'
-end
-
 GOVUKDesignSystemFormBuilder::FormBuilder.class_eval do
   require 'form_builder_helper'
   include FormBuilderHelper
