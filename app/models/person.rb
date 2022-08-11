608e8b109aadd23ddb6bb5555411130a5737f1c3
willmcb <william.mcbrien@digital.justice.gov.uk>
2022-08-11 11:53:40 +0100

    Add #has_home_address? to person + spec

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,14 @@
 class Person < ApplicationRecord
   belongs_to :crime_application
   has_many :addresses, dependent: :destroy
+
+  def has_home_address?
+    home_address.address_line_one.present?
+  end
+
+  private
+
+  def home_address
+    addresses.find_by(type: 'HomeAddress')
+  end
 end
---
+++ spec/models/person_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe Person, type: :model do
+  subject { described_class.new(attributes) }
+
+  let(:attributes) { {} }
+
+  let(:mock_home_address) { 
+    instance_double(
+      'Address', 
+      address_line_one: address_line_one
+    )
+  }
+
+
+  before do
+    allow(subject).to receive(:home_address).and_return(mock_home_address)
+  end
+
+  describe '#has_home_address?' do
+    context 'home address is not blank' do
+
+      let(:address_line_one) { '1 North Pole' }
+
+      it 'returns true when a home address has values' do
+        expect(subject.has_home_address?).to be(true)
+      end
+    end
+
+    context 'home address has is blank' do
+
+      let(:address_line_one) { nil }
+
+      it 'returns false when a home address does not have values' do
+        expect(subject.has_home_address?).to be(false)
+      end
+    end
+  end
+end
