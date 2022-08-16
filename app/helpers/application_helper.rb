e618849c1617ea606a6339a65ead3d14e060c551
willmcb <william.mcbrien@digital.justice.gov.uk>
2022-08-16 16:15:11 +0100

    Basic index page
    Also adds a full_name method to Person + view helper for govuk date
    formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,4 +20,8 @@ module ApplicationHelper
   def decorate(model, decorator_class = nil)
     (decorator_class || [model.class, :Decorator].join.demodulize.constantize).new(model)
   end
+
+  def govuk_formatted_date(date)
+    date.strftime('%d %B %Y')
+  end
 end
