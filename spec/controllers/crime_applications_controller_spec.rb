0f7ed6045579c59715b98a73141397099b4e0c0b
Jesus Laiz <zheileman@users.noreply.github.com>
2022-08-18 12:12:07 +0100

    Return applications having applicants with name
    This improves a bit the dashboard `#index` (list) action so that it only returns applications that have advanced enough to at the very least have an applicant with a name, as otherwise the dashboard either blows app (if there is no applicant record yet) or shows a blank name, no link (if the applicant has no name).
    
    Move the index controller test to a `request` test `spec/requests/dashboard_spec.rb` as it makes a lot of sense for this to be a request test, and much easier to test (although slower I reckon as we are not mocking and we are using real records).

--- spec/controllers/crime_applications_controller_spec.rb
+++ spec/controllers/crime_applications_controller_spec.rb
@@ -1,14 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe CrimeApplicationsController, type: :controller do
-
-  describe '#index' do
-    let(:applications) { CrimeApplication.all } 
-
-    it 'creates @applications' do
-      get :index
-      expect(@controller.instance_variable_get(:@applications)).to match(applications)
-    end
-  end
-
 end
