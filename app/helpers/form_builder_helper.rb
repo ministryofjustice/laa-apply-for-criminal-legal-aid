093d97e9ff4e1d73a779cac212cbf07682b8c67c
Jesus Laiz <zheileman@users.noreply.github.com>
2022-08-18 14:51:46 +0100

    Hide secondary button on some steps
    We decided it doesn't make too much sense to show the "Save and come back later" button right from the beginning when the provider didn't enter anything yet.
    
    As we are taking the provider back to the task list, at least we want to wait until past the client details step so they enter first name / last name and there is something to show if they get back to their list of applications.
    
    Made it so we can disable the secondary action in specific steps by passing `secondary: false`. Default it to show secondary action.

--- app/helpers/form_builder_helper.rb
+++ app/helpers/form_builder_helper.rb
@@ -5,19 +5,11 @@
 module FormBuilderHelper
   def continue_button(primary: :save_and_continue, secondary: :save_and_come_back_later)
     submit_button(primary) do
-      submit_button(secondary, secondary: true, name: 'commit_draft') if show_secondary_button?
+      submit_button(secondary, secondary: true, name: 'commit_draft') if secondary
     end
   end
 
   private
-
-  # The `save and come back later` might not always be needed,
-  # this method can have logic for show/hide.
-  # :nocov:
-  def show_secondary_button?
-    true
-  end
-  # :nocov:
 
   def submit_button(i18n_key, opts = {}, &block)
     govuk_submit I18n.t("helpers.submit.#{i18n_key}"), **opts, &block
