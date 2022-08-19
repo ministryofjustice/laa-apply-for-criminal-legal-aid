6e93fda82c647ee379cbc5e8510bb7cfd6a34e61
Jesus Laiz <zheileman@users.noreply.github.com>
2022-08-19 15:17:37 +0100

    Remove controller specs and use requests instead
    We now have all functionality on the `requests` specs so we can delete the controller spec.

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -12,6 +12,31 @@ RSpec.describe 'Dashboard' do
     end
   end
 
+  describe 'edit an application (aka task list)' do
+    before :all do
+      # sets up a test record
+      app = CrimeApplication.create
+
+      Applicant.create(crime_application: app, first_name: 'Jane', last_name: 'Doe')
+    end
+
+    after :all do
+      CrimeApplication.destroy_all
+    end
+
+    it 'shows the task list for the application' do
+      applicant = Applicant.find_by(first_name: 'Jane')
+      app = applicant.crime_application
+
+      get edit_crime_application_path(app)
+
+      expect(response).to have_http_status(:success)
+
+      assert_select 'h1', 'Make a new application'
+      assert_select 'h2', 'Application incomplete'
+    end
+  end
+
   describe 'list of applications' do
     before :all do
       # sets up a few test records
@@ -44,7 +69,6 @@ RSpec.describe 'Dashboard' do
         end
       end
     end
-
   end
 
   describe 'deleting applications' do
@@ -53,12 +77,6 @@ RSpec.describe 'Dashboard' do
       app = CrimeApplication.create
 
       Applicant.create(crime_application: app, first_name: 'Jane', last_name: 'Doe')
-
-      # sets up a valid session
-      get '/steps/client/has_partner'
-
-      # page actually under test
-      get '/crime_applications'
     end
 
     after :all do
@@ -69,23 +87,30 @@ RSpec.describe 'Dashboard' do
       applicant = Applicant.find_by(first_name: "Jane")
       app = applicant.crime_application
 
-      get "/crime_applications/#{app.id}/confirm_destroy"
-      expect(response.body).to include("Are you sure you want to delete #{applicant.full_name}&#39;s application?")
+      get confirm_destroy_crime_application_path(app)
+
+      expect(response.body).to include("Are you sure you want to delete #{applicant.full_name}’s application?")
       expect(response.body).to include("Yes, delete it")
       expect(response.body).to include("No, do not delete it")
     end
-
 
     it 'can delete an application' do
       applicant = Applicant.find_by(first_name: "Jane")
       app = applicant.crime_application
 
       expect {
-        delete "/crime_applications/#{app.id}"
+        delete crime_application_path(app)
       }.to change { CrimeApplication.count }.by(-1)
 
       expect(response).to have_http_status(:redirect)
       expect(response).to redirect_to(crime_applications_path)
+
+      follow_redirect!
+
+      assert_select 'div.govuk-notification-banner--success', 1 do
+        assert_select 'h2', 'Success'
+        assert_select 'h3', 'Jane Doe’s application has been permanently deleted'
+      end
     end
   end
 end
