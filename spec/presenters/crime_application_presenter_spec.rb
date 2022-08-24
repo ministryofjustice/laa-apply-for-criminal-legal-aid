2b3e649b5897214d505138fc2d19d72c82355079
willmcb <william.mcbrien@digital.justice.gov.uk>
2022-08-24 10:23:21 +0100

    use 'subject' pattern rather than named variable in spec

--- spec/presenters/crime_application_presenter_spec.rb
+++ spec/presenters/crime_application_presenter_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe CrimeApplicationPresenter do
-  let(:presented_application) { 
+  let(:subject) { 
     described_class.new(
       CrimeApplication.new(
         status: 'in_progress', 
@@ -13,31 +13,31 @@ RSpec.describe CrimeApplicationPresenter do
 
   describe 'when presenting CrimeApplications' do
     it 'delegates full name to applicant' do
-      allow(presented_application).to receive(:applicant).and_return(applicant)
+      allow(subject).to receive(:applicant).and_return(applicant)
 
       expect(applicant).to receive(:full_name)
-      expect(presented_application.full_name).to eq('John Doe')
+      expect(subject.full_name).to eq('John Doe')
     end
 
-    it 'can output the presented_application start date in the correct format' do
-      expect(presented_application.start_date).to eq('12 January 2022')
+    it 'can output the subject start date in the correct format' do
+      expect(subject.start_date).to eq('12 January 2022')
     end
 
     it 'has an LAA an reference stubbed' do
-      expect(presented_application.laa_reference).to eq('LAA-a1234b')
+      expect(subject.laa_reference).to eq('LAA-a1234b')
     end
 
     describe 'status tags' do
       it 'can output an in progress tag' do
         tag = '<strong class="govuk-tag govuk-tag--blue">In progress</strong>'
-        expect(presented_application.status_tag).to eq(tag)
+        expect(subject.status_tag).to eq(tag)
       end
 
       it 'can output an in submitted tag' do
-        allow(presented_application).to receive(:status).and_return('submitted')
+        allow(subject).to receive(:status).and_return('submitted')
         tag = '<strong class="govuk-tag govuk-tag--green">Submitted</strong>'
 
-        expect(presented_application.status_tag).to eq(tag)
+        expect(subject.status_tag).to eq(tag)
       end
     end
   end
