cde952785ade847925993b5ddb515c4c1acaa702
willmcb <william.mcbrien@digital.justice.gov.uk>
2022-08-30 14:18:33 +0100

    add casetype value object + spec

---
+++ app/value_objects/case_type.rb
@@ -0,0 +1,11 @@
+class CaseType < ValueObject
+  VALUES = [
+    SUMMARY_ONLY = new(:summary_only),
+    EITHER_WAY = new(:either_way),
+    INTICTABLE = new(:indictable),
+    ALREADY_CC_TRIAL = new(:already_cc_trial),
+    COMMITTAL = new(:committal),
+    CC_APPEAL = new(:cc_appeal),
+    CC_APPEAL_FIN_CHANGE = new(:cc_appeal_fin_change),
+  ].freeze
+end
