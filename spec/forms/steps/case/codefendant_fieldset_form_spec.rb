f0ecc625be3e3dd249b38cf4e3848e934e407697
Jesus Laiz <zheileman@users.noreply.github.com>
2022-09-02 10:26:34 +0100

    CRIMAP-110 Add conflict of interest radio options (#92)
    Another follow-up to all previous PRs related to co-defendants.
    
    This I think is the last one. We add the conflict of interest yes-no radios.
    
    Pretty simple PR as almost everything has been done already in previous PRs.
    
    Only pain was configuring the `govuk_collection_radio_buttons`, because we are using nested attributes, the locales are not automagically fetched for us and we must pass them explicitly to the legend and to both options (the yes and the no).
    
    I've raised an issue in the `govuk-formbuilder` gem repository for a possible enhancement that would make these nested attribute locales work automagically too so we could simplify a lot the code in this view if that happens.

--- spec/forms/steps/case/codefendant_fieldset_form_spec.rb
+++ spec/forms/steps/case/codefendant_fieldset_form_spec.rb
@@ -6,12 +6,22 @@ RSpec.describe Steps::Case::CodefendantFieldsetForm do
     id: record_id,
     first_name: 'John',
     last_name: 'Doe',
+    conflict_of_interest: conflict_of_interest,
   } }
 
   let(:crime_application) { instance_double(CrimeApplication) }
   let(:record_id) { '12345' }
+  let(:conflict_of_interest) { nil }
 
   subject { described_class.new(arguments) }
+
+  describe '#choices' do
+    it 'returns the possible choices' do
+      expect(
+        subject.choices
+      ).to eq([YesNoAnswer::YES, YesNoAnswer::NO])
+    end
+  end
 
   describe '#persisted?' do
     context 'when the record has an ID' do
@@ -27,5 +37,29 @@ RSpec.describe Steps::Case::CodefendantFieldsetForm do
   context 'validations' do
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
+
+    context 'when `conflict_of_interest` is not provided' do
+      it 'has a validation error on the field' do
+        expect(subject).to_not be_valid
+        expect(subject.errors.of_kind?(:conflict_of_interest, :inclusion)).to eq(true)
+      end
+    end
+
+    context 'when `conflict_of_interest` is not valid' do
+      let(:conflict_of_interest) { 'maybe' }
+
+      it 'has a validation error on the field' do
+        expect(subject).to_not be_valid
+        expect(subject.errors.of_kind?(:conflict_of_interest, :inclusion)).to eq(true)
+      end
+    end
+
+    context 'when `conflict_of_interest` is valid' do
+      let(:conflict_of_interest) { 'yes' }
+
+      it 'has no validation error' do
+        expect(subject).to be_valid
+      end
+    end
   end
 end
