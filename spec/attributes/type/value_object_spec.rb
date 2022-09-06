3a077a22b82c940cb8b4b2b762c6353981b1afcb
Jesus Laiz <zheileman@users.noreply.github.com>
2022-09-06 09:20:41 +0100

    Replace `yes_no` custom attribute with generic one
    The `yes_no` custom attribute is a QoL or syntax sugar addition to facilitate automatic casting of the commonly used yes-no radios.
    
    This makes for simpler form objects, with less boilerplate code and more granted for free. For example there is no need to use a `#string_choices` method to validate inclusion, etc.
    
    With these changes, we are removing the `yes_no` attribute but adding a new `value_object` attribute. This new attribute is generic and can be passed the "source" class of the values. All the rest behaves the same.

---
+++ spec/attributes/type/value_object_spec.rb
@@ -0,0 +1,87 @@
+require 'rails_helper'
+
+RSpec.describe Type::ValueObject do
+  # NOTE: we will test this Type with `YesNoAnswer` as the source,
+  # but any other value-object behaves the same, no need to test them all.
+  subject { described_class.new(source: YesNoAnswer) }
+
+  let(:coerced_value) { subject.cast(value) }
+
+  describe 'registry' do
+    it 'is registered with type `:value_object`' do
+      expect(
+        ActiveModel::Type.lookup(:value_object).is_a?(described_class)
+      ).to eq(true)
+    end
+
+    it 'has an underlying type of `:value_object`' do
+      expect(subject.type).to eq(:value_object)
+    end
+  end
+
+  describe 'when value is `nil`' do
+    let(:value) { nil }
+    it { expect(coerced_value).to be_nil }
+  end
+
+  describe 'when value is a symbol' do
+    let(:value) { :yes }
+    it { expect(coerced_value).to eq(YesNoAnswer::YES) }
+  end
+
+  describe 'when value is a string' do
+    let(:value) { 'yes' }
+    it { expect(coerced_value).to eq(YesNoAnswer::YES) }
+  end
+
+  describe 'when value is already a value-object' do
+    let(:value) { YesNoAnswer::YES }
+    it { expect(coerced_value).to eq(YesNoAnswer::YES) }
+  end
+
+  describe '#serialize' do
+    let(:value) { YesNoAnswer::NO }
+
+    it 'casts a value from the ruby type to a type that the database knows how to understand' do
+      expect(
+        subject.serialize(value)
+      ).to eq('no')
+    end
+  end
+
+  describe '#type_cast_for_schema' do
+    let(:value) { YesNoAnswer::NO }
+
+    it 'casts a value for schema dumping' do
+      expect(
+        subject.type_cast_for_schema(value)
+      ).to eq('no')
+    end
+  end
+
+  describe 'identity methods' do
+    let(:type_one) { described_class.new(source: String) }
+    let(:type_two) { described_class.new(source: String) }
+    let(:type_three) { described_class.new(source: DateTime) }
+
+    describe '#==' do
+      it 'considers same type/same source equal' do
+        expect(type_one).to eq(type_two)
+      end
+
+      it 'considers same type/different source not equal' do
+        expect(type_one).to_not eq(type_three)
+      end
+    end
+
+    describe '#hash' do
+      it 'considers same type/same source equal' do
+        expect(type_one.hash).to eq(type_two.hash)
+      end
+
+      it 'considers same type/different source not equal' do
+        expect(type_one.hash).to_not eq(type_three.hash)
+      end
+    end
+  end
+end
