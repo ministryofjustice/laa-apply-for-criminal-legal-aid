690c4ff991a77461b1c7c09b00babf09a2ed3e37
Jesus Laiz <zheileman@users.noreply.github.com>
2022-09-12 17:32:56 +0100

    CRIMAP-125 Initial commit of offences CSV
    Store the CSV file containing the pre-filtered list of offences and their attributes, and a thin layer to read it and perform basic operations like find by code (or by name if that make sense), and retrieve all offences.
    
    This is inspired by HMCTS common platform with a very similar approach:
    https://github.com/ministryofjustice/hmcts_common_platform
    
    I've made some small tweaks and modifications to better fit our purposes (also implemented the equality methods), but overall is exactly the same.
    
    We might not need the Offences DB table anymore but for now I'm not doing anything on that front until we've validated this other alternative (CSV file).
    
    I've just reused the same `Offence` model but removed its ActiveRecord capabilities so it is just a simple PORO/entity for now.

---
+++ app/models/concerns/csv_queryable.rb
@@ -0,0 +1,56 @@
+require 'csv'
+
+module CsvQueryable
+  extend ActiveSupport::Concern
+
+  included do
+    attr_reader :row
+  end
+
+  def initialize(row:)
+    @row = row
+  end
+
+  def ==(other)
+    other.is_a?(self.class) && other.row == row
+  end
+  alias === ==
+  alias eql? ==
+
+  def hash
+    [self.class, row].hash
+  end
+
+  class_methods do
+    def csv_filepath(filepath)
+      self.filepath = filepath
+    end
+
+    def csv_attributes(*attrs)
+      attrs.each do |name|
+        define_method(name) { row[name.to_s].strip }
+      end
+    end
+
+    def find(code)
+      find_by(code:)
+    end
+
+    def find_by(**query)
+      attr, value = query.first
+      all.find { |obj| obj.public_send(attr) == value }
+    end
+
+    def all
+      @all ||= csv.map { |row| new(row:) }
+    end
+
+    private
+
+    attr_accessor :filepath
+
+    def csv
+      CSV.read(@filepath, headers: true)
+    end
+  end
+end
--- spec/models/offence_spec.rb
+++ spec/models/offence_spec.rb
@@ -1,14 +1,80 @@
 require 'rails_helper'
 
 RSpec.describe Offence, type: :model do
-  subject { described_class.new(attributes) }
-  let(:attributes) { {} }
+  describe '.find' do
+    it 'is a short form of `find_by(code:)`' do
+      expect(described_class).to receive(:find_by).with(code: 'FA06001')
+      described_class.find('FA06001')
+    end
+  end
 
-  describe '#code' do
-    let(:attributes) { { id: 'ABC123'} }
+  describe '.find_by' do
+    context 'can search by code' do
+      subject { described_class.find_by(code: 'FA06001') }
 
-    it 'is an alias of the `id` attribute' do
-      expect(subject.code).to eq('ABC123')
+      it 'returns the offence found' do
+        expect(subject).to be_a(described_class)
+      end
+
+      it 'has the offence attributes' do
+        expect(subject.code).to eq('FA06001')
+        expect(subject.name).to eq('Fraud by false representation')
+        expect(subject.offence_class).to eq('F/G/K')
+        expect(subject.offence_type).to eq('CE Either Way')
+      end
+    end
+
+    context 'can search by name' do
+      subject { described_class.find_by(name: 'Fraud by false representation') }
+
+      it 'returns the offence found' do
+        expect(subject.code).to eq('FA06001')
+      end
+    end
+
+    context 'when no offence is found' do
+      subject { described_class.find_by(code: 'XYZ123') }
+      it { is_expected.to be_nil }
+    end
+  end
+
+  describe '.all' do
+    subject { described_class.all }
+
+    it 'returns all offences' do
+      expect(subject).to all(be_a(described_class))
+      expect(subject.size).to eq(233)
+    end
+
+    it 'keeps the same order as in the CSV file' do
+      expect(subject.first.code).to eq('TH68010')
+      expect(subject.last.code).to eq('TH68006A')
+    end
+  end
+
+  describe 'equality methods' do
+    let(:offence_one) { described_class.new(row: 'row_A') }
+    let(:offence_two) { described_class.new(row: 'row_A') }
+    let(:offence_three) { described_class.new(row: 'row_B') }
+
+    describe '#==' do
+      it 'considers same type/same row equal' do
+        expect(offence_one).to eq(offence_two)
+      end
+
+      it 'considers same type/different row not equal' do
+        expect(offence_one).to_not eq(offence_three)
+      end
+    end
+
+    describe '#hash' do
+      it 'considers same type/same row equal' do
+        expect(offence_one.hash).to eq(offence_two.hash)
+      end
+
+      it 'considers same type/different row not equal' do
+        expect(offence_one.hash).to_not eq(offence_three.hash)
+      end
     end
   end
 end
