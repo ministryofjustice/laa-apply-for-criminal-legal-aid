87a84d74493d7ba5cad89091da634f4553c432f7
willmcb <william.mcbrien@digital.justice.gov.uk>
2022-09-16 11:14:18 +0100

    User let record_id

--- spec/forms/steps/case/offence_date_fieldset_form_spec.rb
+++ spec/forms/steps/case/offence_date_fieldset_form_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 RSpec.describe Steps::Case::OffenceDateFieldsetForm do
   let(:arguments) { {
     crime_application: crime_application,
-    record: OffenceDate.new(date: "02, 07, 2020")
+    record: OffenceDate.new(date: "02, 07, 2020"),
+    id: record_id
   } }
 
+  let(:record_id) { '123456' } 
   let(:crime_application) { instance_double(CrimeApplication) }
 
   subject { described_class.new(arguments) }
@@ -17,13 +19,12 @@ RSpec.describe Steps::Case::OffenceDateFieldsetForm do
   describe '#persisted?' do
     context 'when form has an id' do
       it 'it returns true' do
-        arguments[:id] = "1232456"
-
         expect(subject.persisted?).to be(true)
       end
     end
 
     context 'when form has no id' do
+      let(:record_id) { nil } 
       it 'it returns false' do
         expect(subject.persisted?).to be(false)
       end
