ca52fe813d84b50ea0e7c6ec957efd6d889e55a5
Arthur Ashman <arthur.ashman@digital.justice.gov.uk>
2022-10-12 15:48:38 +0100

    Add ioj_type value object

---
+++ spec/value_objects/ioj_type_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe IojType do
+  subject { described_class.new(value) }
+
+  let(:value) { :foo }
+
+  describe '.values' do
+    it 'returns all possible values' do
+      expect(described_class.values.map(&:to_s)).to eq(
+        %w(
+          loss_of_liberty
+          suspended_sentence
+          loss_of_livelyhood
+          reputation
+          question_of_law
+          understanding
+          witness_tracing
+          expert_examination
+          interest_of_another
+          other
+        )
+      )
+    end
+  end
+end
