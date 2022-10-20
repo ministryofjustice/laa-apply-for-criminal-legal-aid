4db1b10b7a794e4da7f3458a220c2fc2caecd91a
Jesus Laiz <zheileman@users.noreply.github.com>
2022-10-20 08:36:13 +0100

    CRIMAP-157 Review offences (#142)
    Add the offences section to the check your answers page.
    
    This renders a slightly different markup as the offences are indexed, and shows more details like the class or the dates.
    
    In order to do this in a clean way, a specific "OffenceAnswer" component has been created, with its corresponding view partial. Other than that it behaves the same as other answers really.

---
+++ app/presenters/summary/sections/offences.rb
@@ -0,0 +1,25 @@
+module Summary
+  module Sections
+    class Offences < Sections::BaseSection
+      def name
+        :offences
+      end
+
+      def answers
+        charges.map.with_index(1) do |charge, index|
+          Components::OffenceAnswer.new(
+            :offence_details, ChargePresenter.new(charge),
+            change_path: edit_steps_case_charges_path(charge),
+            i18n_opts: { index: }
+          )
+        end.select(&:show?)
+      end
+
+      private
+
+      def charges
+        @charges ||= crime_application.case.charges
+      end
+    end
+  end
+end
