8b1b66971a018f2e347b065adb1ba1b798edf3a5
Arthur Ashman <arthur.ashman@digital.justice.gov.uk>
2022-11-22 17:33:34 +0000

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_12_172446) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_22_170409) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
