4cf43b7b8eefba943dee560231bf2cc96c1dc1d3
kennyevil <christopher.gannon@digital.justice.gov.uk>
2023-02-07 15:46:35 +0000

    add eforms redirect for users when DWP is unreachable

--- app/controllers/steps/client/retry_benefit_check_controller.rb
+++ app/controllers/steps/client/retry_benefit_check_controller.rb
@@ -3,6 +3,12 @@ module Steps
     class RetryBenefitCheckController < Steps::ClientStepController
       include Steps::NoOpAdvanceStep
 
+      def update
+        return redirect_to Settings.eforms_url, allow_other_host: true if params.key?(:commit_draft)
+
+        super
+      end
+
       private
 
       def advance_as
--- spec/controllers/steps/client/retry_benefit_check_controller_spec.rb
+++ spec/controllers/steps/client/retry_benefit_check_controller_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Steps::Client::RetryBenefitCheckController, type: :controller do
   it_behaves_like 'a no-op advance step controller', :retry_benefit_check, Decisions::ClientDecisionTree
+
+  describe '#update' do
+    context 'when the user chooses to apply eforms' do
+      let(:existing_case) { CrimeApplication.create(applicant: Applicant.new) }
+
+      it 'redirects to the eforms site' do
+        put :update, params: { id: existing_case, commit_draft: '' }
+        expect(response).to redirect_to(Settings.eforms_url)
+      end
+    end
+  end
 end
