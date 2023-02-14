1514421241c807cdb54805fbbad7144134200f93
Jesus Laiz <zheileman@users.noreply.github.com>
2023-02-14 14:34:07 +0000

    Enable JWT authentication

--- config/initializers/datastore_client.rb
+++ config/initializers/datastore_client.rb
@@ -4,11 +4,12 @@ DatastoreApi.configure do |config|
   config.api_root = ENV.fetch('DATASTORE_API_ROOT', nil)
   config.api_path = '/api/v2'
 
-  # Basic auth is only needed on staging
-  # To be replaced with `jwt` auth soon
-  config.auth_type = :basic
-  config.basic_auth_username = ENV.fetch('DATASTORE_AUTH_USERNAME', nil)
-  config.basic_auth_password = ENV.fetch('DATASTORE_AUTH_PASSWORD', nil)
+  config.auth_type = :jwt
+
+  # Keeping this for some time in case we need to revert to
+  # http basic auth (`config.auth_type = :basic`)
+  # config.basic_auth_username = ENV.fetch('DATASTORE_AUTH_USERNAME', nil)
+  # config.basic_auth_password = ENV.fetch('DATASTORE_AUTH_PASSWORD', nil)
 
   config.logger = Logger.new(STDOUT)
   config.logger.level = Logger::WARN
