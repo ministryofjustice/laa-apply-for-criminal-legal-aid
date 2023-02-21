14f898694f00fb31afd028f446b64d4d9ea1535e
Jesus Laiz <zheileman@users.noreply.github.com>
2023-02-21 10:55:41 +0000

    CRIMAP-286 Configure logging and filtering of PII
    Logs to stdout so other systems can route these logs to final destinations for viewing and long-term archival.
    For example this is needed to see the logs in Kibana.
    
    Also, added PII parameters to the filter. These are also used by Sentry when reporting exceptions.

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -5,5 +5,22 @@
 # notations and behaviors.
 Rails.application.config.filter_parameters += [
   :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn,
-  :SAMLResponse, :Signature, :SigAlg, :KeyInfo
+  :SAMLResponse, :Signature, :SigAlg, :KeyInfo,
+  # Attributes relating to an application
+  # It does partial matching (i.e. `telephone_number` is covered by `phone`)
+  :first_name,
+  :last_name,
+  :other_names,
+  :date_of_birth,
+  :nino,
+  :address_line,
+  :postcode,
+  :email,
+  :phone,
+  :urn,
+  :maat_id,
+  :reason,
+  :description,
+  :justification,
+  :appeal_with_changes_details,
 ]
