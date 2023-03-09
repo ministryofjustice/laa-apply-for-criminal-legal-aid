13038eb524cbc051f2c240af912cc61efc6d8b94
Jesus Laiz <zheileman@users.noreply.github.com>
2023-03-09 16:31:23 +0000

    CRIMAP-279 Cleanup datastore http auth leftovers
    We implemented JWT tokens some time ago, this just clean ups some left overs and secrets no longer needed.
    
    Note: Review may want to do the same.

--- config/initializers/datastore_client.rb
+++ config/initializers/datastore_client.rb
@@ -6,11 +6,6 @@ DatastoreApi.configure do |config|
 
   config.auth_type = :jwt
 
-  # Keeping this for some time in case we need to revert to
-  # http basic auth (`config.auth_type = :basic`)
-  # config.basic_auth_username = ENV.fetch('DATASTORE_AUTH_USERNAME', nil)
-  # config.basic_auth_password = ENV.fetch('DATASTORE_AUTH_PASSWORD', nil)
-
   config.logger = Logger.new(STDOUT)
   config.logger.level = Logger::WARN
 end
