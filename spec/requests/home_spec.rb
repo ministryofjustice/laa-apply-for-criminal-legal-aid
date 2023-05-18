d5d6541d78be9cc86628c8e75b6cdbf679d727a1
Jesus Laiz <zheileman@users.noreply.github.com>
2023-05-18 11:32:26 +0100

    Tweaks to header colour handling and smoke test (#347)
    A few tweaks that makes it more robust.
    
    The CSS will in effect use the orange colour for any environment unless it is production (this is better than having to specify one by one the non-prod envs).
    
    The phase tag text will also default to whatever is the name of the env, unless overridden by locales (as it is the case of `production` -> `alpha`).
    This is also better than having to specify one by one all the env names, as usually the phase tag just shows the same (`staging` -> `staging`, `local` -> `local`).
    
    Added a simple smoke test to ensure the CSS classes are applied correctly and the phase tag text is also correct.

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -3,8 +3,46 @@ require 'rails_helper'
 RSpec.describe 'Home' do
   describe 'index' do
     it 'renders the expected page' do
-      get '/'
+      get root_path
       expect(response).to have_http_status(:ok)
+    end
+  end
+
+  # Basic smoke test
+  describe 'customisation of header and phase tag' do
+    context 'for `test` env name' do
+      before do
+        get root_path
+      end
+
+      it 'has the correct body css classes' do
+        assert_select 'body.app-body.app-body--test'
+      end
+
+      it 'has the correct phase tag' do
+        assert_select 'body.app-body.app-body--test' do
+          assert_select '.govuk-phase-banner__content__tag', 'test'
+        end
+      end
+    end
+
+    context 'for `production` env name' do
+      before do
+        allow(Rails.env).to receive(:test?).and_return(false)
+        allow(ENV).to receive(:fetch).with('ENV_NAME').and_return('production')
+
+        get root_path
+      end
+
+      it 'has the correct body css classes' do
+        assert_select 'body.app-body.app-body--production'
+      end
+
+      it 'has the correct phase tag' do
+        assert_select 'body.app-body.app-body--production' do
+          assert_select '.govuk-phase-banner__content__tag', 'alpha'
+        end
+      end
     end
   end
 end
