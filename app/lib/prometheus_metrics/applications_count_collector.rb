3d6f56008b495f15d92e4f3ce99279a67ff58cb5
Jesus Laiz <zheileman@users.noreply.github.com>
2023-06-26 10:26:24 +0100

    Refactor application counters to use tags
    This is more in line with how prometheus likes this kind of metrics and also makes it easier to visualise in graphs.
    
    This is now the result:
    
    ```
    # HELP ruby_crime_apply_applications_count Number of applications by status
    # TYPE ruby_crime_apply_applications_count counter
    ruby_crime_apply_applications_count{status="started"} 8
    
    # HELP ruby_crime_apply_applications_count Number of applications by status
    # TYPE ruby_crime_apply_applications_count counter
    ruby_crime_apply_applications_count{status="in_progress"} 6
    
    # HELP ruby_crime_apply_applications_count Number of applications by status
    # TYPE ruby_crime_apply_applications_count counter
    ruby_crime_apply_applications_count{status="date_stamped"} 5
    
    # HELP ruby_crime_apply_applications_count Number of applications by status
    # TYPE ruby_crime_apply_applications_count counter
    ruby_crime_apply_applications_count{status="stale"} 3
    ```

--- app/lib/prometheus_metrics/applications_count_collector.rb
+++ app/lib/prometheus_metrics/applications_count_collector.rb
@@ -25,60 +25,46 @@ module PrometheusMetrics
 
     private
 
-    def total_count
-      counter = counter(
-        'crime_apply_total_applications_count',
-        'Number of application records'
+    def counter
+      PrometheusExporter::Metric::Counter.new(
+        'crime_apply_applications_count', 'Number of applications by status'
       )
+    end
 
+    def total_count
       result = Rails.cache.fetch(__method__, expires_in:) { CrimeApplication.count }
 
-      counter.observe(result)
-      counter
+      gauge = counter
+      gauge.observe(result, status: 'started')
+      gauge
     end
 
     def in_progress_count
-      counter = counter(
-        'crime_apply_in_progress_applications_count',
-        'Number of in progress applications'
-      )
-
       result = Rails.cache.fetch(__method__, expires_in:) { CrimeApplication.with_applicant.count }
 
-      counter.observe(result)
-      counter
+      gauge = counter
+      gauge.observe(result, status: 'in_progress')
+      gauge
     end
 
     def date_stamped_count
-      counter = counter(
-        'crime_apply_date_stamped_applications_count',
-        'Number of date stamped applications'
-      )
-
       result = Rails.cache.fetch(__method__, expires_in:) do
         CrimeApplication.where.not(date_stamp: nil).count
       end
 
-      counter.observe(result)
-      counter
+      gauge = counter
+      gauge.observe(result, status: 'date_stamped')
+      gauge
     end
 
     def stale_count
-      counter = counter(
-        'crime_apply_stale_applications_count',
-        'Number of stale applications (not updated for more than 1 week)'
-      )
-
       result = Rails.cache.fetch(__method__, expires_in:) do
         CrimeApplication.with_applicant.where(['crime_applications.updated_at < ?', 1.week.ago]).count
       end
 
-      counter.observe(result)
-      counter
-    end
-
-    def counter(name, help)
-      PrometheusExporter::Metric::Counter.new(name, help)
+      gauge = counter
+      gauge.observe(result, status: 'stale')
+      gauge
     end
   end
 end
