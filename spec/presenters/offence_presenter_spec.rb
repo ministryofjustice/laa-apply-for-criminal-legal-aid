b8f336e178a89ab4c10edcb6ae7b82f7f76be8b5
Jesus Laiz <zheileman@users.noreply.github.com>
2023-06-29 16:47:52 +0100

    CRIMAP-426 PoC offence list synonyms (#413)
    The suggestions component we are using for the offences list, comes with a synonym capability, so for instance entering a synonym will return the “expanded“ offence from the list.
    
    We just needed to "create" these synonyms.
    
    This is a functional PoC.
    
    The synonyms are separate and not coupled to our offences list, and work more like a progressive enhancement (because it will only work with the javascript version of the suggestions list anyways).
    
    Everything else will continue the same, the synonym is just a quick way to bring up the “expanded / long“ offence name in the dropdown for providers to select.

--- spec/presenters/offence_presenter_spec.rb
+++ spec/presenters/offence_presenter_spec.rb
@@ -4,13 +4,37 @@ RSpec.describe OffencePresenter do
   let(:offence) do
     instance_double(
       Offence,
-      offence_class: 'H'
+      name:,
+      offence_class:,
     )
   end
+
+  let(:name) { 'Foobar offence' }
+  let(:offence_class) { 'H' }
 
   describe '#offence_class' do
     subject { described_class.new(offence).offence_class }
 
     it { is_expected.to eq('Class H') }
+  end
+
+  describe '#synonyms' do
+    subject { described_class.new(offence).synonyms }
+
+    before do
+      allow(OffenceSynonyms).to receive(:lookup).with(name).and_return(synonyms)
+    end
+
+    context 'when no synonyms are found' do
+      let(:synonyms) { [] }
+
+      it { is_expected.to be_nil }
+    end
+
+    context 'when synonyms are found' do
+      let(:synonyms) { %w[one another] }
+
+      it { is_expected.to eq('one|another') }
+    end
   end
 end
