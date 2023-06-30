b7d092fd8a951b03eff1e50b244ea65ae1bbcef8
Jesus Laiz <zheileman@users.noreply.github.com>
2023-06-30 13:48:36 +0100

    Improved CSP policy rules
    Defined missing `base_uri` (to `none`). This one, I thought was being inherited from the `default_src` rule, but it has no inheritance so has to be defined explicitly.
    
    More strict `script_src` policy, by favouring nonces over domain host lists.
    
    Only very old browsers that only support CSP1 might have issues as these don't support nonces. But it is a tiny percentage according to current browser stats.
    
    This page came handy to test the policy:
    https://csp-evaluator.withgoogle.com

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -7,12 +7,13 @@
 Rails.application.configure do
   config.content_security_policy do |policy|
     policy.default_src :none
+    policy.base_uri    :none
     policy.style_src   :self
     policy.font_src    :self, :data
     policy.img_src     :self, :data, 'https://*.google-analytics.com', 'https://*.googletagmanager.com'
     policy.connect_src :self, 'https://ga.jspm.io', 'https://*.google-analytics.com', 'https://*.analytics.google.com', 'https://*.googletagmanager.com'
-    policy.script_src  :self, 'https://ga.jspm.io', 'https://*.googletagmanager.com'
     policy.form_action :self, 'https://*.legalservices.gov.uk/oamfed/idp/samlv20'
+    policy.script_src  :strict_dynamic
 
     # Specify URI for violation reports
     # policy.report_uri "/csp-violation-report-endpoint"
