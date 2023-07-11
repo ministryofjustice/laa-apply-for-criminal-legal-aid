791a95edb9f078905678ca3ad67d9d57911b2973
Jesus Laiz <zheileman@users.noreply.github.com>
2023-07-11 16:34:34 +0100

    Remove `hmcts_common_platform` gem dependency
    Based on some recent conversations, and the fact the repo for `hmcts_common_platform` is archived and apparently unmaintained and outdated, one immediate easy thing we can do is bring over the (simplified) CSV file and use the same approach we used for offences.
    
    Functionally everything should continue working as before, and we have more flexibility to update or tweak the CSV file to add/remove or rename courts.
    
    In the future we can plan to move to an API based approach using FaCT.

--- spec/models/court_spec.rb
+++ spec/models/court_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Court, type: :model do
+  describe '.find_by_name' do
+    it 'is shorthand for `.find_by`' do
+      expect(described_class).to receive(:find_by).with(name: 'Foobar')
+      described_class.find_by(name: 'Foobar')
+    end
+  end
+
   describe '.all' do
     subject(:all) { described_class.all }
 
