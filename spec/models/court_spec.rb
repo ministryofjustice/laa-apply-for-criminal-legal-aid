5e8beda5b3710366a3d4310b011f90d0428a67da
Jesus Laiz <zheileman@users.noreply.github.com>
2023-07-12 08:30:47 +0100

    Remove dup Shrewsbury Crown Court

--- spec/models/court_spec.rb
+++ spec/models/court_spec.rb
@@ -11,8 +11,12 @@ RSpec.describe Court, type: :model do
   describe '.all' do
     subject(:all) { described_class.all }
 
+    it 'returns all courts' do
+      expect(all.size).to eq(249)
+    end
+
     it 'returns required courts as expected' do
-      digest_of_expected_court_names = '4cdc39a4fa9cfe2aeb4984fe1dbab5d1'
+      digest_of_expected_court_names = '2783b31a61666a79b810dc8ef95c61d1'
 
       expect(Digest::MD5.hexdigest(all.map(&:name).join)).to eq digest_of_expected_court_names
     end
