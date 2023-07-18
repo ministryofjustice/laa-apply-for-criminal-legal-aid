7e856fe4777163a77120d213fc8beb71138b69db
Jesus Laiz <zheileman@users.noreply.github.com>
2023-07-18 15:16:36 +0100

    Set `Cache-Control` response header to no-store
    It avoids possibly exposing personal information when using the browser back button after the current session has been terminated.
    
    This is similar to the approach taken on Review however instead of adding it to all controller actions, this is only set on those exposing possible personal information.
    
    There are many other pages on Apply (all footer pages, and some others) that are not sensitive.
    
    Note pragma header not set as is deprecated and no longer recommended.
    https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Pragma

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,10 @@ class DashboardController < ApplicationController
   helper_method :in_progress_count, :returned_count, :sortable_columns
   delegate :returned_count, to: :application_counters
 
+  # Avoid the browser caching any of the dashboard pages so the
+  # back button after signing out can't load any sensitive details
+  before_action :no_store
+
   private
 
   # Implement in sub-controllers to narrow down allowed columns
