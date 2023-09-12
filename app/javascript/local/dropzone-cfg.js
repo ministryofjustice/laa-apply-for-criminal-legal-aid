61cf85df3f7c42562d2f3f6d1bd3b7cf19584077
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2023-09-12 13:17:23 +0100

    Display dropbox file preview and rename spec file to remove warning

--- app/javascript/local/dropzone-cfg.js
+++ app/javascript/local/dropzone-cfg.js
@@ -25,6 +25,62 @@ DropzoneCfg.prototype.init = function () {
   this.$dropzoneContainerText = document.querySelector('div.dz-message')
   this.$chooseFilesButton = document.querySelector('button#choose_files_button')
   this.$dropzoneContainerText.appendChild(this.$chooseFilesButton)
+
+  this.$dropzone.on('addedfile', (file, response) => {
+    let row = createTableRow(file);
+    self.$feedbackContainer.querySelector('.govuk-table__body').append(row);
+  });
+
+  this.$dropzone.on('success', (file, response) => {
+    this.$statusTag = document.getElementById(file.upload.uuid).querySelector(".govuk-tag")
+    this.$statusTag.classList.remove("govuk-tag--yellow")
+    this.$statusTag.classList.add("govuk-tag--green")
+    this.$statusTag.innerHTML = "Uploaded"
+  });
+
+  this.$dropzone.on('error', (file, response) => {
+    let error = createErrorMessage(response.error_message)
+    this.$tableCell = document.getElementById(file.upload.uuid)
+    this.$tableCell.classList.add("govuk-form-group--error")
+    this.$tableCell.append(error)
+    this.$statusTag = this.$tableCell.querySelector(".govuk-tag")
+    this.$tableCell.removeChild(this.$statusTag)
+  })
+}
+function createTableRow(file) {
+  let row = document.createElement("tr")
+  let cell = document.createElement("td")
+  let statusTag = createStatusTag("Uploading")
+
+  cell.setAttribute("id", file.upload.uuid)
+
+  row.classList.add("govuk-table__row")
+  cell.classList.add("govuk-table__cell")
+
+  cell.append(file.name)
+  cell.append(statusTag)
+  row.append(cell)
+  return row
 }
 
+function createStatusTag(text) {
+  let tag = document.createElement("strong")
+  tag.classList.add("govuk-tag", "govuk-tag--yellow")
+  tag.textContent = text
+  return tag
+}
+
+function createErrorMessage (msg) {
+  const errorEl = document.createElement("p")
+  errorEl.classList.add("govuk-error-message")
+
+  // to assist screen reader users
+  const screenReaderError = document.createElement("span")
+  screenReaderError.classList.add("govuk-visually-hidden")
+  screenReaderError.textContent = "Error:"
+
+  errorEl.textContent = msg
+  errorEl.prepend(screenReaderError)
+  return errorEl
+}
 export default DropzoneCfg
