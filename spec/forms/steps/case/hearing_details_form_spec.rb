f2e75b0d410973d4b5766defa173d52e6bd2af8f
Jesus Laiz <zheileman@users.noreply.github.com>
2023-10-02 11:08:22 +0100

    CRIMAP-598 First court hearing selection
    Follow-up to PR #493 to expose the radio options in the existing hearing details page, for the provider to select whether the first hearing court is the same as the next hearing court and logic to move to next page.
    
    Includes validation as well as logic to delete previous answer if the provider changes the radio selection.
    
    This is now part of the wider journey, but there is not Check Your Answer yet, that will be part of the next PR.
    Also missing submission to datastore/rehydration.
    
    This is not behind a feature flag as I'm hoping to finish it soonish enough to be able to test and release as is a minor iteration on current form.

--- spec/forms/steps/case/hearing_details_form_spec.rb
+++ spec/forms/steps/case/hearing_details_form_spec.rb
@@ -6,17 +6,45 @@ RSpec.describe Steps::Case::HearingDetailsForm do
   let(:arguments) do
     {
       crime_application: crime_application,
-    hearing_court_name: 'Cardiff Court',
-    hearing_date: Date.tomorrow,
+      hearing_court_name: 'Cardiff Court',
+      hearing_date: Date.tomorrow,
+      is_first_court_hearing: is_first_court_hearing,
     }
   end
 
   let(:crime_application) { instance_double(CrimeApplication) }
+  let(:is_first_court_hearing) { FirstHearingAnswer::YES.to_s }
+
+  describe '#choices' do
+    it 'returns the possible choices' do
+      expect(
+        subject.choices
+      ).to eq([FirstHearingAnswer::YES, FirstHearingAnswer::NO, FirstHearingAnswer::NO_HEARING_YET])
+    end
+  end
 
   describe '#save' do
     context 'validations' do
       it { is_expected.to validate_presence_of(:hearing_court_name) }
       it { is_expected.to validate_presence_of(:hearing_date) }
+
+      context 'when `is_first_court_hearing` is not provided' do
+        let(:is_first_court_hearing) { '' }
+
+        it 'has a validation error on the field' do
+          expect(subject).not_to be_valid
+          expect(subject.errors.of_kind?(:is_first_court_hearing, :inclusion)).to be(true)
+        end
+      end
+
+      context 'when `is_first_court_hearing` is not valid' do
+        let(:is_first_court_hearing) { 'foobar' }
+
+        it 'has a validation error on the field' do
+          expect(subject).not_to be_valid
+          expect(subject.errors.of_kind?(:is_first_court_hearing, :inclusion)).to be(true)
+        end
+      end
     end
 
     context 'hearing_date' do
@@ -25,13 +53,31 @@ RSpec.describe Steps::Case::HearingDetailsForm do
                       allow_past: false, allow_future: true
     end
 
+    # rubocop:disable Style/HashSyntax
     context 'when validations pass' do
-      it_behaves_like 'a has-one-association form',
-                      association_name: :case,
-                      expected_attributes: {
-                        'hearing_court_name' => 'Cardiff Court',
-                        'hearing_date' => Date.tomorrow,
-                      }
+      context 'when it is the first court hearing it resets dependent attributes' do
+        it_behaves_like 'a has-one-association form',
+                        association_name: :case,
+                        expected_attributes: {
+                          'hearing_court_name' => 'Cardiff Court',
+                          'hearing_date' => Date.tomorrow,
+                          'is_first_court_hearing' => FirstHearingAnswer::YES,
+                          first_court_hearing_name: nil,
+                        }
+      end
+
+      context 'when it is not the first court hearing' do
+        let(:is_first_court_hearing) { FirstHearingAnswer::NO.to_s }
+
+        it_behaves_like 'a has-one-association form',
+                        association_name: :case,
+                        expected_attributes: {
+                          'hearing_court_name' => 'Cardiff Court',
+                          'hearing_date' => Date.tomorrow,
+                          'is_first_court_hearing' => FirstHearingAnswer::NO,
+                        }
+      end
     end
+    # rubocop:enable Style/HashSyntax
   end
 end
