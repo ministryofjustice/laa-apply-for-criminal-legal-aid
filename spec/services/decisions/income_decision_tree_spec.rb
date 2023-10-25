b7841083dc974318cb36cf63d0a26604ff192a41
Arthur Ashman <45827968+arthurashman@users.noreply.github.com>
2023-10-25 11:47:25 +0200

    Add did_client_lose_job_being_in_custody step (#515)
    Add new step behind feature flag
    Add to income decision tree
    Add route with alias, controller, form and errors

--- spec/services/decisions/income_decision_tree_spec.rb
+++ spec/services/decisions/income_decision_tree_spec.rb
@@ -3,5 +3,27 @@ require 'rails_helper'
 RSpec.describe Decisions::IncomeDecisionTree do
   subject { described_class.new(form_object, as: step_name) }
 
+  let(:crime_application) do
+    instance_double(
+      CrimeApplication,
+      id: 'uuid',
+    )
+  end
+
+  before do
+    allow(
+      form_object
+    ).to receive(:crime_application).and_return(crime_application)
+  end
+
   it_behaves_like 'a decision tree'
+
+  context 'when the step is `lost_job_in_custody`' do
+    let(:form_object) { double('FormObject') }
+    let(:step_name) { :lost_job_in_custody }
+
+    context 'has correct next step' do
+      it { is_expected.to have_destination('/home', :index, id: crime_application) }
+    end
+  end
 end
