8ce109be1e0ae6664ba9215ec2fd24584aa525c4
willmcb <william.mcbrien@digital.justice.gov.uk>
2023-11-09 13:41:44 +0000

    Amendments from PR review

--- app/forms/steps/income/income_before_tax_form.rb
+++ app/forms/steps/income/income_before_tax_form.rb
@@ -4,7 +4,7 @@ module Steps
       include Steps::HasOneAssociation
       has_one_association :income_details, through: :applicant
 
-      # threshold being £12,475 currently
+      # threshold being £12,475 as of Nov 2023
       attribute :income_above_threshold, :value_object, source: YesNoAnswer
 
       validates_inclusion_of :income_above_threshold, in: :choices
