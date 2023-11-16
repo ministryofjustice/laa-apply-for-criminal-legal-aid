091049dfda4ceb695c372dc6c7cdc92f46c2a926
Arthur Ashman <arthur.ashman@digital.justice.gov.uk>
2023-11-16 13:28:12 +0000

    Serialize 12475 answer

--- app/serializers/submission_serializer/sections/means_details.rb
+++ app/serializers/submission_serializer/sections/means_details.rb
@@ -4,9 +4,10 @@ module SubmissionSerializer
       def to_builder
         Jbuilder.new do |json|
           json.means_details do
-            json.income_details do
-              income_above_threshold(json)
-              lost_job_in_custody(json)
+            if crime_application.income_details
+              json.income_details do
+                json.income_above_threshold crime_application.income_details.income_above_threshold
+              end
             end
           end
         end
