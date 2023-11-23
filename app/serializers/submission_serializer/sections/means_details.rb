a975696aa33239b9e519102acfc5c3e29df2a84c
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2023-11-23 16:06:00 +0000

    CRIMAP-129 Upgrade schema gem 1.0.9

--- app/serializers/submission_serializer/sections/means_details.rb
+++ app/serializers/submission_serializer/sections/means_details.rb
@@ -1,13 +1,15 @@
 module SubmissionSerializer
   module Sections
     class MeansDetails < Sections::BaseSection
-      def to_builder
+      def to_builder # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
         Jbuilder.new do |json|
           json.means_details do
-            if crime_application.income_details
-              json.income_details do
+            json.income_details do
+              if crime_application.income_details
                 json.income_above_threshold crime_application.income_details.income_above_threshold
+              end
 
+              if crime_application.applicant&.lost_job_in_custody
                 json.lost_job_in_custody crime_application.applicant.lost_job_in_custody
                 json.date_job_lost crime_application.applicant.date_job_lost
               end
