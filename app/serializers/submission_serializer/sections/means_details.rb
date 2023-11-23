4cf005c2887a8342af41dbe84d7c1f3642d47e01
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2023-11-23 16:57:06 +0000

    CRIMAP-129 Refactor serializer

--- app/serializers/submission_serializer/sections/means_details.rb
+++ app/serializers/submission_serializer/sections/means_details.rb
@@ -1,21 +1,30 @@
 module SubmissionSerializer
   module Sections
     class MeansDetails < Sections::BaseSection
-      def to_builder # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+      def to_builder
         Jbuilder.new do |json|
           json.means_details do
             json.income_details do
-              if crime_application.income_details
-                json.income_above_threshold crime_application.income_details.income_above_threshold
-              end
-
-              if crime_application.applicant&.lost_job_in_custody
-                json.lost_job_in_custody crime_application.applicant.lost_job_in_custody
-                json.date_job_lost crime_application.applicant.date_job_lost
-              end
+              income_above_threshold(json)
+              lost_job_in_custody(json)
             end
           end
         end
+      end
+
+      private
+
+      def income_above_threshold(json)
+        return unless crime_application.income_details
+
+        json.income_above_threshold crime_application.income_details.income_above_threshold
+      end
+
+      def lost_job_in_custody(json)
+        return unless crime_application.applicant&.lost_job_in_custody
+
+        json.lost_job_in_custody crime_application.applicant.lost_job_in_custody
+        json.date_job_lost crime_application.applicant.date_job_lost
       end
     end
   end
