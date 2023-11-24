b28a6523307e61c8ae10e7e9011c3f302751cacb
Mo Seedat <47113046+mseedat-moj@users.noreply.github.com>
2023-11-24 16:14:20 +0000

    [CRIMAP-105/129] Lost job in custody store/rehydrate (#547)
    * CRIMAP-129 Add lost job
    
    * CRIMAP-129 Update schema to newest
    
    * CRIMAP-129 Upgrade schema gem 1.0.9
    
    * CRIMAP-129 Add new means details spec
    
    * CRIMAP-129 Refactor serializer
    
    * CRIMAP-129 Rehydrate applicant

--- app/serializers/submission_serializer/sections/means_details.rb
+++ app/serializers/submission_serializer/sections/means_details.rb
@@ -4,13 +4,27 @@ module SubmissionSerializer
       def to_builder
         Jbuilder.new do |json|
           json.means_details do
-            if crime_application.income_details
-              json.income_details do
-                json.income_above_threshold crime_application.income_details.income_above_threshold
-              end
+            json.income_details do
+              income_above_threshold(json)
+              lost_job_in_custody(json)
             end
           end
         end
+      end
+
+      private
+
+      def income_above_threshold(json)
+        return unless crime_application.income_details
+
+        json.income_above_threshold crime_application.income_details.income_above_threshold
+      end
+
+      def lost_job_in_custody(json)
+        return unless crime_application.applicant&.lost_job_in_custody
+
+        json.lost_job_in_custody crime_application.applicant.lost_job_in_custody
+        json.date_job_lost crime_application.applicant.date_job_lost
       end
     end
   end
