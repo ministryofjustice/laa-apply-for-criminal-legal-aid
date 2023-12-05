0ec2748673b18aeac759e7825a71f27a317fbcef
Arthur Ashman <45827968+arthurashman@users.noreply.github.com>
2023-12-05 10:50:40 +0100

    [CRIMAPP-116] Rehydrate/Show employment details in CYA (#567)
    Show employment details in CYA

--- spec/presenters/summary/sections/employment_details_spec.rb
+++ spec/presenters/summary/sections/employment_details_spec.rb
@@ -14,6 +14,8 @@ describe Summary::Sections::EmploymentDetails do
   let(:income) do
     instance_double(
       Income,
+      employment_status: ['not_working'],
+      ended_employment_within_three_months: 'yes',
       lost_job_in_custody: 'yes',
       date_job_lost: Date.new(2023, 11, 20),
     )
@@ -44,15 +46,23 @@ describe Summary::Sections::EmploymentDetails do
 
     context 'when there are income details' do
       it 'has the correct rows' do
-        expect(answers.count).to eq(2)
+        expect(answers.count).to eq(4)
         expect(answers[0]).to be_an_instance_of(Summary::Components::ValueAnswer)
-        expect(answers[0].question).to eq(:lost_job_in_custody)
-        expect(answers[0].change_path).to match('applications/12345/steps/income/did_client_lose_job_being_in_custody')
-        expect(answers[0].value).to eq('yes')
-        expect(answers[1]).to be_an_instance_of(Summary::Components::DateAnswer)
-        expect(answers[1].question).to eq(:date_job_lost)
-        expect(answers[1].change_path).to match('applications/12345/steps/income/did_client_lose_job_being_in_custody')
-        expect(answers[1].value).to eq(Date.new(2023, 11, 20))
+        expect(answers[0].question).to eq(:employment_status)
+        expect(answers[0].change_path).to match('applications/12345/steps/income/what_is_clients_employment_status')
+        expect(answers[0].value).to eq('not_working')
+        expect(answers[1]).to be_an_instance_of(Summary::Components::ValueAnswer)
+        expect(answers[1].question).to eq(:ended_employment_within_three_months)
+        expect(answers[1].change_path).to match('applications/12345/steps/income/what_is_clients_employment_status')
+        expect(answers[1].value).to eq('yes')
+        expect(answers[2]).to be_an_instance_of(Summary::Components::ValueAnswer)
+        expect(answers[2].question).to eq(:lost_job_in_custody)
+        expect(answers[2].change_path).to match('applications/12345/steps/income/did_client_lose_job_being_in_custody')
+        expect(answers[2].value).to eq('yes')
+        expect(answers[3]).to be_an_instance_of(Summary::Components::DateAnswer)
+        expect(answers[3].question).to eq(:date_job_lost)
+        expect(answers[3].change_path).to match('applications/12345/steps/income/did_client_lose_job_being_in_custody')
+        expect(answers[3].value).to eq(Date.new(2023, 11, 20))
       end
     end
   end
