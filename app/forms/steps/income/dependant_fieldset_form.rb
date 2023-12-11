893d95811b067f34dcd714378bfd2528b046afda
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2023-12-11 10:18:36 +0000

    CRIMAP-106 Ensure age is over 0

--- app/forms/steps/income/dependant_fieldset_form.rb
+++ app/forms/steps/income/dependant_fieldset_form.rb
@@ -5,7 +5,11 @@ module Steps
       attribute :id, :string
       attribute :age, :integer
 
-      validates :age, presence: true, numericality: { less_than: Dependant::MAX_AGE, only_integer: true }
+      validates :age, presence: true, numericality: {
+        greater_than_or_equal_to: 0,
+        less_than: Dependant::MAX_AGE,
+        only_integer: true
+      }
 
       # Needed for `#fields_for` to render the uuids as hidden fields
       def persisted?
