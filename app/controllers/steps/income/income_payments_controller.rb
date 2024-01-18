b564a7d938b8972e94b6988492e897a2713d511f
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2024-01-18 14:04:05 +0000

    Cleanup controller

--- app/controllers/steps/income/income_payments_controller.rb
+++ app/controllers/steps/income/income_payments_controller.rb
@@ -14,12 +14,12 @@ module Steps
       end
 
       def additional_permitted_params
-        #[income_payments_attributes: {  }, income_payments: []]
-        #[income_payments_attributes: {  }, types: [], income_payments: []]
-
-        #[(IncomePaymentType.values.map { |t| [t.to_s, Steps::Income::IncomePaymentFieldsetForm.attribute_names] } + [['income_payments', []]]).to_h]
-        #[(IncomePaymentType.values.map { |t| [t.to_s, Steps::Income::IncomePaymentFieldsetForm.attribute_names] } + [['payment_types', []]]).to_h]
-        [(IncomePaymentType.values.map { |t| [t.to_s, Steps::Income::IncomePaymentFieldsetForm.attribute_names] } + [['types', []]] + [['income_payments', []]]).to_h]
+        [(
+            IncomePaymentType.values.map { |t| [t.to_s, Steps::Income::IncomePaymentFieldsetForm.attribute_names] } +
+            [['types', []]] +
+            [['income_payments', []]]
+          ).to_h
+        ]
       end
     end
   end
