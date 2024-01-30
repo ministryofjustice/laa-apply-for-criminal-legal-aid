a97280e103dfec64d4a9fa4c50eb5f7b3dcf46b7
timpeat <tim@timpeat.com>
2024-01-30 08:39:19 +0000

    CRIMAPP-420 update after PSE content review (#618)
    * CRIMAPP-420 update after PSE content review

--- spec/services/passporting/means_passporter_spec.rb
+++ spec/services/passporting/means_passporter_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 RSpec.describe Passporting::MeansPassporter do
   subject { described_class.new(crime_application) }
 
-  let(:crime_application) { instance_double(CrimeApplication, applicant:, parent_id:) }
+  let(:crime_application) { instance_double(CrimeApplication, applicant: applicant, resubmission?: resubmission?) }
+
   let(:applicant) { instance_double(Applicant, under18?: under18, passporting_benefit: passporting_benefit) }
 
-  let(:parent_id) { nil }
+  let(:resubmission?) { false }
   let(:under18) { nil }
   let(:passporting_benefit) { nil }
 
@@ -18,7 +19,7 @@ RSpec.describe Passporting::MeansPassporter do
 
   describe '#call' do
     context 'for a resubmitted application' do
-      let(:parent_id) { 'uuid' }
+      let(:resubmission?) { true }
 
       it 'uses the existing values' do
         expect(crime_application).not_to receive(:update)
@@ -118,7 +119,7 @@ RSpec.describe Passporting::MeansPassporter do
     end
 
     context 'for a resubmitted application' do
-      let(:parent_id) { 'uuid' }
+      let(:resubmission?) { true }
 
       before do
         allow(crime_application).to receive(:means_passport).and_return(means_passport)
@@ -154,7 +155,7 @@ RSpec.describe Passporting::MeansPassporter do
     end
 
     context 'for a resubmitted application' do
-      let(:parent_id) { 'uuid' }
+      let(:resubmission?) { true }
 
       before do
         allow(crime_application).to receive(:means_passport).and_return(means_passport)
