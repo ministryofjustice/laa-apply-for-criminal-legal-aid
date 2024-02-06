392b4d117068acd06728096490d5d63ac2a2e91e
timpeat <tim@timpeat.com>
2024-02-06 16:16:40 +0000

    asdf

--- app/models/partner_details.rb
+++ app/models/partner_details.rb
@@ -2,14 +2,14 @@ class PartnerDetails < ApplicationRecord
   belongs_to :partner
 
   def has_contrary_interests?
-    return true if conflict_of_interest == YesNoAnswer::YES
+    return true if YesNoAnswer.new(conflict_of_interest) == YesNoAnswer::YES
     return true if victim_or_prosecution_witness?
 
     false
   end
 
   def involved_in_case?
-    return false if involvement_in_case != InvolvementInCase::NONE
+    return false if InvolvementInCase.new(involvement_in_case) != InvolvementInCase::NONE
 
     true
   end
