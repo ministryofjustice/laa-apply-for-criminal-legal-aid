52097d8356ea6c18776111e215f2a99b3dc72d30
Ripan <er.ripankumar@gmail.com>
2024-02-19 17:40:57 +0000

    Update property_type_form_spec.rb

--- spec/forms/steps/capital/property_type_form_spec.rb
+++ spec/forms/steps/capital/property_type_form_spec.rb
@@ -3,18 +3,6 @@ require 'rails_helper'
 RSpec.describe Steps::Capital::PropertyTypeForm do
   subject(:form) { described_class.new }
 
-  # let(:arguments) do
-  #   {
-  #     crime_application:,
-  #     property_type:
-  #   }
-  # end
-  #
-  # let(:crime_application) { instance_double(CrimeApplication, properties:) }
-  # let(:properties) { Property.new }
-  #
-  # let(:property_type) { nil }
-
   describe '#choices' do
     it 'returns the possible choices' do
       expect(
