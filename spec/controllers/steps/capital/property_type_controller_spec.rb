4bbb19b32c46b7e76cb4b768a9fa222add40c338
Ripan <er.ripankumar@gmail.com>
2024-02-19 17:13:45 +0000

    Update property_type_controller_spec.rb

--- spec/controllers/steps/capital/property_type_controller_spec.rb
+++ spec/controllers/steps/capital/property_type_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Steps::Capital::PropertiesController, type: :controller do
+RSpec.describe Steps::Capital::PropertyTypeController, type: :controller do
   it_behaves_like 'a generic step controller', Steps::Capital::PropertyTypeForm,
                   Decisions::CapitalDecisionTree
 end
