b38d33a5a6e41eb010a8ed4426bb030e47431086
Arthur Ashman <45827968+arthurashman@users.noreply.github.com>
2024-02-23 10:53:40 +0000

    Connect income journey to flow (#641)
    Connect income journey to follow on from benefit type question

--- app/services/decisions/outgoings_decision_tree.rb
+++ app/services/decisions/outgoings_decision_tree.rb
@@ -10,8 +10,7 @@ module Decisions
       when :income_tax_rate
         edit(:outgoings_more_than_income)
       when :outgoings_more_than_income
-        # TODO: link to next step when we have it
-        show('/home', action: :index)
+        edit('/steps/case/urn')
       else
         raise InvalidStep, "Invalid step '#{step_name}'"
       end
