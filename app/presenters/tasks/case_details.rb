317d41903dac9baab72e110f6822d3dc34904087
Ripan <er.ripankumar@gmail.com>
2024-02-27 15:58:23 +0000

    refactor and add specs

--- app/presenters/tasks/case_details.rb
+++ app/presenters/tasks/case_details.rb
@@ -18,12 +18,7 @@ module Tasks
     end
 
     def completed?
-      crime_application.case.values_at(
-        :hearing_court_name, :hearing_date
-      ).all?(&:present?) &&
-        crime_application.case.case_concluded_attributes_present? &&
-        crime_application.case.client_remanded_attributes_present? &&
-        crime_application.case.preorder_work_attributes_present?
+      crime_application.case.complete?
     end
   end
 end
