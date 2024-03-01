06b0590db94a859250715c788486baca93ac7cf4
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2024-03-01 13:05:57 +0000

    Add other saving type form

--- app/controllers/steps/capital/other_saving_type_controller.rb
+++ app/controllers/steps/capital/other_saving_type_controller.rb
@@ -1,6 +1,15 @@
 module Steps
   module Capital
     class OtherSavingTypeController < SavingTypeController
+      def edit
+        @form_object = OtherSavingTypeForm.build(
+          current_crime_application
+        )
+      end
+
+      def update
+        update_and_advance(OtherSavingTypeForm, as: :saving_type)
+      end
     end
   end
 end
