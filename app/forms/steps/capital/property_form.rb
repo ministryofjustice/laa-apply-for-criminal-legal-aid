10d0e01b7f12ccc7c2a6a9d4004a22db68f834dc
Ripan <er.ripankumar@gmail.com>
2024-03-06 18:47:25 +0000

    Refactor and add specs

--- app/forms/steps/capital/property_form.rb
+++ app/forms/steps/capital/property_form.rb
@@ -3,8 +3,6 @@ module Steps
     class PropertyForm < Steps::BaseFormObject
       delegate :property_type, to: :record
 
-      CUSTOM_HOUSE_TYPE = 'custom'.freeze
-
       attribute :house_type, :string
       attribute :custom_house_type, :string
       attribute :bedrooms, :integer
