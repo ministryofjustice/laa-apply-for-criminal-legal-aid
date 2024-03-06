03ce97bf079f7361098b31239e6a37c8e8bba022
Ripan <er.ripankumar@gmail.com>
2024-03-06 15:06:14 +0000

    Refactor and fix specs

--- app/controllers/steps/capital/properties_controller.rb
+++ app/controllers/steps/capital/properties_controller.rb
@@ -2,6 +2,12 @@ module Steps
   module Capital
     class PropertiesController < Steps::CapitalStepController
       include Steps::Capital::PropertyUpdateStep
+
+      private
+
+      def flash_msg
+        nil
+      end
     end
   end
 end
