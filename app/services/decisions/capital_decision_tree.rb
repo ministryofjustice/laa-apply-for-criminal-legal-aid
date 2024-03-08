2a9bff22a43604a23fade08c03407f80fa707155
Ripan <er.ripankumar@gmail.com>
2024-03-08 17:54:57 +0000

    Add specs

--- app/services/decisions/capital_decision_tree.rb
+++ app/services/decisions/capital_decision_tree.rb
@@ -105,7 +105,6 @@ module Decisions
         property_owners.create! if incomplete_property_owners.blank?
         return edit(:property_owners, property_id: property)
       end
-      # TODO: Route to appropriate property page loop once built
       edit(:properties_summary) # Placeholder to join up flow
     end
 
@@ -114,8 +113,6 @@ module Decisions
         property_owners.create! if incomplete_property_owners.blank?
         return edit(:property_owners, property_id: property)
       end
-
-      # TODO: Route to appropriate property page loop once built
       edit(:properties_summary) # Placeholder to join up flow
     end
 
