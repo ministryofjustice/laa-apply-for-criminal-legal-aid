91bf126d3103353d52158635a7c529033c6e72b0
Ripan <er.ripankumar@gmail.com>
2024-03-12 16:51:33 +0000

    Create property_spec.rb

---
+++ spec/serializers/submission_serializer/definitions/property_spec.rb
@@ -0,0 +1,86 @@
+require 'rails_helper'
+
+RSpec.describe SubmissionSerializer::Definitions::Property do
+  subject { described_class.generate(savings) }
+
+  let(:savings) { [property1, property2] }
+
+  let(:property1) do
+    instance_double(
+      Property,
+      {
+        property_type: 'residential',
+        house_type: 'bungalow',
+        custom_house_type: nil,
+        size_in_acres: 100,
+        usage: 'usage details',
+        bedrooms: 2,
+        value_before_type_cast: 300_000,
+        outstanding_mortgage_before_type_cast: 100_000,
+        percentage_applicant_owned: 20,
+        percentage_partner_owned: nil,
+        is_home_address: 'yes',
+        has_other_owners: 'no',
+        address: { 'city' => 'London', 'postcode' => 'TW7' }
+      }
+    )
+  end
+
+  let(:property2) do
+    instance_double(
+      Property,
+      property_type: 'residential',
+      house_type: 'custom',
+      custom_house_type: 'custom house type',
+      size_in_acres: 200,
+      usage: 'usage details',
+      bedrooms: 2,
+      value_before_type_cast: 300_000,
+      outstanding_mortgage_before_type_cast: 100_000,
+      percentage_applicant_owned: 70,
+      percentage_partner_owned: 30,
+      is_home_address: 'yes',
+      has_other_owners: 'no',
+      address: nil
+    )
+  end
+
+  let(:json_output) do
+    [
+      {
+        property_type: 'residential',
+        house_type: 'bungalow',
+        custom_house_type: nil,
+        size_in_acres: 100,
+        usage: 'usage details',
+        bedrooms: 2,
+        value: 300_000,
+        outstanding_mortgage: 100_000,
+        percentage_applicant_owned: 20,
+        percentage_partner_owned: nil,
+        is_home_address: 'yes',
+        has_other_owners: 'no',
+        address: { 'city' => 'London', 'postcode' => 'TW7' }
+      },
+      {
+        property_type: 'residential',
+        house_type: 'custom',
+        custom_house_type: 'custom house type',
+        size_in_acres: 200,
+        usage: 'usage details',
+        bedrooms: 2,
+        value: 300_000,
+        outstanding_mortgage: 100_000,
+        percentage_applicant_owned: 70,
+        percentage_partner_owned: 30,
+        is_home_address: 'yes',
+        has_other_owners: 'no',
+        address: nil
+      },
+    ].as_json
+  end
+
+  describe '#generate' do
+    it { expect(subject).to eq(json_output) }
+  end
+end
