12a731ff8008d7de6a5854da5f0999aff25d98bf
Ripan <er.ripankumar@gmail.com>
2024-03-12 17:29:51 +0000

    change percentage columns to decimals

--- db/schema.rb
+++ db/schema.rb
@@ -247,7 +247,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_18_112707) do
     t.uuid "crime_application_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "payment_type"
+    t.string "payment_type", null: false
     t.integer "amount"
     t.string "frequency"
     t.jsonb "metadata", default: {}
@@ -271,7 +271,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_18_112707) do
     t.boolean "passporting_benefit"
     t.string "benefit_type"
     t.string "has_benefit_evidence"
-    t.index ["type", "crime_application_id"], name: "index_people_on_type_and_crime_application_id", unique: true
+    t.index ["crime_application_id"], name: "index_people_on_crime_application_id", unique: true
   end
 
   create_table "properties", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
