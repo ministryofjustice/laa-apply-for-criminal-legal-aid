c71ae7fdb56040466aade076f57f1f0f76a6537a
Ripan <er.ripankumar@gmail.com>
2024-04-08 15:51:24 +0100

    Update income_decision_tree.rb

--- app/services/decisions/income_decision_tree.rb
+++ app/services/decisions/income_decision_tree.rb
@@ -31,7 +31,15 @@ module Decisions
       when :manage_without_income
         edit(:answers)
       when :answers
-        determine_continuing_means_journey
+        routes = Rails.application.routes.url_helpers
+        if previous_step_path.in? [
+                                    routes.edit_steps_income_employment_status_path(crime_application),
+                                    routes.edit_steps_income_lost_job_in_custody_path(crime_application)
+                                  ]
+          continuing_evidence_upload
+        else
+          determine_continuing_means_journey
+        end
       else
         raise InvalidStep, "Invalid step '#{step_name}'"
       end
@@ -40,11 +48,17 @@ module Decisions
 
     private
 
+    def previous_step_path
+      # Second to last element in the array, will be nil for arrays of size 0 or 1
+      current_crime_application&.navigation_stack&.slice(-2) || root_path
+    end
+
     def after_employment_status
       if not_working?
         if ended_employment_within_three_months?
           edit(:lost_job_in_custody)
         elsif appeal_no_changes?
+          # edit('/steps/evidence/upload')
           edit(:answers)
         else
           edit(:income_before_tax)
@@ -57,6 +71,7 @@ module Decisions
 
     def after_lost_job_in_custody
       if appeal_no_changes?
+        # edit('/steps/evidence/upload')
         edit(:answers)
       else
         edit(:income_before_tax)
@@ -172,6 +187,10 @@ module Decisions
       else
         edit('/steps/evidence/upload')
       end
+    end
+
+    def continuing_evidence_upload
+      edit('/steps/evidence/upload')
     end
   end
 end
