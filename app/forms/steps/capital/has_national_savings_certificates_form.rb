e54179e119c72d7ca8bdee0462646ce00c1a3c0e
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2024-04-11 11:58:26 +0100

    Undo delete code

--- app/forms/steps/capital/has_national_savings_certificates_form.rb
+++ app/forms/steps/capital/has_national_savings_certificates_form.rb
@@ -15,19 +15,12 @@ module Steps
       def persist!
         capital.update(attributes)
 
-        return delete_national_savings_certificates if has_national_savings_certificates.no?
-
         @national_savings_certificate = incomplete_national_savings_certificate ||
                                         NationalSavingsCertificate.create!(crime_application:)
       end
 
       def incomplete_national_savings_certificate
         crime_application.national_savings_certificates.reject(&:complete?).first
-      end
-
-      def delete_national_savings_certificates
-        crime_application.national_savings_certificates.destroy_all
-        true
       end
     end
   end
