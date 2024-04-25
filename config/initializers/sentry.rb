7b538da67396c67b38b11741ecab546387e0e360
Arthur Ashman <45827968+arthurashman@users.noreply.github.com>
2024-04-25 07:43:38 +0100

    update sentry secrets (#802)

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,8 +1,6 @@
 Rails.application.config.to_prepare do
   # Sentry is enabled if SENTRY_DSN environment variable is set
   Sentry.init do |config|
-    config.traces_sample_rate = 0.05
-    config.dsn = 'https://78db26a7cadc95e7dacce11f2852a240@o345774.ingest.us.sentry.io/4507142444023808'
     config.breadcrumbs_logger = [:active_support_logger, :http_logger]
     config.environment = HostEnv.env_name
 
