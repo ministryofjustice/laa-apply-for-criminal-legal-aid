1e5f59e4da7178659bda3fd4331b95c53d2a3585
timpeat <tim@timpeat.com>
2024-04-26 09:55:13 +0100

    VLKJ

--- spec/validators/client_details/answers_validator_spec.rb
+++ spec/validators/client_details/answers_validator_spec.rb
@@ -4,11 +4,46 @@ RSpec.describe ClientDetails::AnswersValidator, type: :model do
   subject(:validator) { described_class.new(record) }
 
   let(:record) { instance_double(CrimeApplication, errors:, applicant:, kase:) }
-  let(:errors) { double(:errors, empty?: errors_empty) }
-  let(:errors_empty) { true }
+  let(:errors) { double(:errors, empty?: false) }
   let(:applicant) { instance_double(Applicant, benefit_type: nil, residence_type: 'house') }
-
   let(:kase) { instance_double(Case, case_type: 'case_type') }
+
+  describe '#validate' do
+    context 'when validation fails' do
+      before do
+        allow(applicant).to receive(:values_at).with(:date_of_birth, :first_name,
+                                                     :last_name).and_return(['2000-11-11', nil, 'Tim'])
+        allow(applicant).to receive_messages(correspondence_address_type: nil, has_nino: nil, has_benefit_evidence: nil)
+        allow(record).to receive(:kase).and_return(nil)
+        allow_any_instance_of(Passporting::MeansPassporter).to receive(:call).and_return(false)
+      end
+
+      let(:attributes) do
+        {
+          employment_status: nil,
+          income_above_threshold: nil,
+          has_frozen_income_or_assets: nil,
+          has_no_income_payments: 'no',
+          has_no_income_benefits: 'no',
+          income_payments: [],
+          income_benefits: [],
+          client_has_dependants: nil,
+          manage_without_income: nil
+        }
+      end
+
+      it 'adds errors for all failed validations' do
+        expect(errors).to receive(:add).with(:details, :blank)
+        expect(errors).to receive(:add).with(:case_type, :blank)
+        expect(errors).to receive(:add).with(:residence_type, :blank)
+        expect(errors).to receive(:add).with(:has_nino, :blank)
+        expect(errors).to receive(:add).with(:benefit_type, :blank)
+        expect(errors).to receive(:add).with(:base, :incomplete_records)
+
+        subject.validate
+      end
+    end
+  end
 
   describe '#address_complete?' do
     before do
@@ -16,8 +51,6 @@ RSpec.describe ClientDetails::AnswersValidator, type: :model do
     end
 
     context 'when we have completed mandatory contact details' do
-      let(:address) { instance_double(Address, address_line_one: 'some address') }
-
       context 'for a home address' do
         let(:correspondence_address_type) { CorrespondenceType::HOME_ADDRESS.to_s }
 
