7340455b2c5141f9d17df9da383ea43ca1d06980
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2024-04-26 17:47:46 +0100

    CRIMAPP-649 Amend income_p60_sa302_2 remove self_assessment

--- app/services/evidence/rules/20240426002316_self_assessed.rb
+++ app/services/evidence/rules/20240426002316_self_assessed.rb
@@ -7,8 +7,11 @@ module Evidence
       group :income
 
       client do |crime_application|
-        crime_application.income&.self_assessment? ||
-          (crime_application.outgoings&.income_tax_rate_above_threshold == 'yes')
+        if crime_application.outgoings
+          (crime_application.outgoings.income_tax_rate_above_threshold == 'yes') || false
+        else
+          false
+        end
       end
 
       # TODO: Awaiting partner implementation
