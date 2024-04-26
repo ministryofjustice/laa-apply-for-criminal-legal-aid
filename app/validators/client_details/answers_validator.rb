c5c583d8252ea2b2625506d0fdb79b92a3f37db8
timpeat <tim@timpeat.com>
2024-04-26 14:16:42 +0100

    CRIMAPP-781 understands when client details complete. (#812)
    * CRIMAPP-783 check passporting for after ioj decision
    
    WIP
    
    wip
    
    * WIP
    
    * VLKJ
    
    * Add client details answers validator
    
    * Set ioj task wqpath according to passport
    
    * Mark assessment as incomplete if client details section is incomplete
    
    * Handle appeal to crown court no changes
    
    * Fix linting error
    
    ---------
    
    Co-authored-by: hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>

---
+++ app/validators/client_details/answers_validator.rb
@@ -0,0 +1,73 @@
+module ClientDetails
+  class AnswersValidator
+    include TypeOfMeansAssessment
+
+    def initialize(record)
+      @record = record
+    end
+
+    attr_reader :record
+
+    delegate :errors, :applicant, :kase, :crime_application, :appeal_no_changes?, to: :record
+
+    # Adds the error to the first step name a user would need to go to
+    # fix the issue.
+
+    def validate # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity:
+      errors.add(:details, :blank) unless applicant_details_complete?
+      errors.add(:case_type, :blank) unless case_type_complete?
+
+      unless appeal_no_changes?
+        errors.add(:residence_type, :blank) unless address_complete?
+        errors.add(:has_nino, :blank) unless has_nino_complete?
+        errors.add(:benefit_type, :blank) unless passporting_complete?
+      end
+
+      errors.add :base, :incomplete_records unless errors.empty?
+    end
+
+    def address_complete?
+      return false if applicant.residence_type.blank?
+
+      case applicant.correspondence_address_type
+      when CorrespondenceType::HOME_ADDRESS.to_s
+        applicant.home_address?
+      when CorrespondenceType::OTHER_ADDRESS.to_s
+        applicant.correspondence_address?
+      when CorrespondenceType::PROVIDERS_OFFICE_ADDRESS.to_s
+        true
+      else
+        false
+      end
+    end
+
+    def applicant_details_complete?
+      applicant.values_at(
+        :date_of_birth, :first_name, :last_name,
+      ).all?(&:present?)
+    end
+
+    def case_type_complete?
+      return false unless kase
+
+      kase.case_type.present?
+    end
+
+    def passporting_complete?
+      return true if applicant.benefit_type == 'none'
+      return true if evidence_of_passporting_means_forthcoming?
+      return true if means_assessment_in_lieu_of_passporting?
+
+      Passporting::MeansPassporter.new(crime_application).call
+    end
+
+    def has_nino_complete?
+      return false if applicant.has_nino.blank?
+      return true if applicant.has_nino == 'no'
+
+      applicant.nino.present?
+    end
+
+    alias crime_application record
+  end
+end
