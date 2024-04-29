31aec295efdbcb0add1936b0d69339179da55b89
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2024-04-29 13:48:22 +0100

    Add rule 31

--- spec/services/evidence/rule_spec.rb
+++ spec/services/evidence/rule_spec.rb
@@ -268,6 +268,7 @@ RSpec.describe Evidence::Rule do
         Evidence::Rules::UnitTrusts,
         Evidence::Rules::InvestmentBonds,
         Evidence::Rules::OtherLumpSums,
+        Evidence::Rules::RestraintOrFreezingOrder,
 
         # Includes test rules in /fixtures
         Evidence::Rules::ExampleRule1,
