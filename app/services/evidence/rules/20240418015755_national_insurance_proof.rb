39336f4253fde65f8db16b4b42cc24d3cefbfa89
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2024-04-29 13:08:32 +0100

    CRIMAPP-649 Add other to client and partner

--- app/services/evidence/rules/20240418015755_national_insurance_proof.rb
+++ app/services/evidence/rules/20240418015755_national_insurance_proof.rb
@@ -6,8 +6,12 @@ module Evidence
       key :national_insurance_32
       group :none
 
-      other do |crime_application|
+      client do |crime_application|
         crime_application&.applicant&.has_nino == 'yes'
+      end
+
+      partner do |crime_application|
+        true
       end
     end
   end
