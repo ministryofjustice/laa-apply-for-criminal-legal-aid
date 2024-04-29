507859aca8e824fa7de1908e29c3b33c413c3435
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2024-04-29 10:01:41 +0100

    CRIMAPP-649 Remove rubocop warning

--- app/models/concerns/person_with_dob.rb
+++ app/models/concerns/person_with_dob.rb
@@ -2,9 +2,7 @@ module PersonWithDob
   extend ActiveSupport::Concern
 
   def under18?
-    if date_of_birth.blank?
-      raise DateOfBirthPending, 'Cannot determine under18 because date of birth is not set'
-    end
+    raise DateOfBirthPending, 'Cannot determine under18 because date of birth is not set' if date_of_birth.blank?
 
     date_of_birth + 18.years > Time.zone.today
   end
