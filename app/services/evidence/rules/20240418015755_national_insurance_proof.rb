8108de47a9ebf9d8b8903fa95b324c1923240757
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2024-04-29 14:27:24 +0100

    CRIMAPP-649 Show other group per persona

--- app/services/evidence/rules/20240418015755_national_insurance_proof.rb
+++ app/services/evidence/rules/20240418015755_national_insurance_proof.rb
@@ -6,12 +6,8 @@ module Evidence
       key :national_insurance_32
       group :none
 
-      client do |crime_application|
+      other do |crime_application|
         crime_application&.applicant&.has_nino == 'yes'
-      end
-
-      partner do |crime_application|
-        true
       end
     end
   end
