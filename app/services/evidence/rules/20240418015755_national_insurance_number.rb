8eee015898ea9ba5d79e61ac897049d645c4e560
Rose <rose.azadkhan@digital.justice.gov.uk>
2024-04-30 16:05:17 +0100

    CRIMAPP-649 Update NINO rule for evidence prompt (#821)
    * CRIMAPP-649 update NINO rule for evidence prompt
    
    * update age rule to fall back to non-date stamp age rule
    
    * Amend date stamp type
    
    * Update upload spec to use legit rule
    
    *  Add comment about which rule should be triggered
    
    * Add method to applicant to check for passporting benefit
    
    ---------
    
    Co-authored-by: RoseSAK <rose.azadkan@digital.justice.gov.uk>
    Co-authored-by: hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
    Co-authored-by: Mo Seedat <mohammed.seedat@digital.justice.gov.uk>

---
+++ app/services/evidence/rules/20240418015755_national_insurance_number.rb
@@ -0,0 +1,19 @@
+module Evidence
+  module Rules
+    class NationalInsuranceNumber < Rule
+      include Evidence::RuleDsl
+
+      key :national_insurance_32
+      group :none
+
+      client do |crime_application|
+        crime_application&.applicant_requires_nino_evidence?
+      end
+
+      # TODO: Awaiting partner implementation
+      partner do |_crime_application|
+        false
+      end
+    end
+  end
+end
---
+++ spec/models/applicant_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe Applicant, type: :model do
+  subject { described_class.new(attributes) }
+
+  let(:attributes) { {} }
+
+  describe '#has_passporting_benefit' do
+    context 'when `benefit_type` is Universal Credit' do
+      let(:attributes) { { benefit_type: 'universal_credit' } }
+
+      it 'returns true' do
+        expect(subject.has_passporting_benefit?).to be true
+      end
+    end
+
+    context 'when `benefit_type` is invalid' do
+      let(:attributes) { { benefit_type: 'invalid_benefit_type' } }
+
+      it 'returns false' do
+        expect(subject.has_passporting_benefit?).to be false
+      end
+    end
+
+    context 'when `benefit_type` is none' do
+      let(:attributes) { { benefit_type: 'none' } }
+
+      it 'returns false' do
+        expect(subject.has_passporting_benefit?).to be false
+      end
+    end
+  end
+end
