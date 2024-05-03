19b425b4c88bfe4a41455395a8fd8db04bf6cab9
Rose <rose.azadkhan@digital.justice.gov.uk>
2024-05-03 11:41:41 +0100

    CRIMAPP-773 Fix task list item for additional information section  (#835)
    CRIMAPP-773 update additional information formand task list item
    
    persist additional_information_required to the db, update task list complete method, refactor form and specs
    
    Co-authored-by: RoseSAK <rose.azadkan@digital.justice.gov.uk>

--- app/forms/steps/submission/more_information_form.rb
+++ app/forms/steps/submission/more_information_form.rb
@@ -1,35 +1,30 @@
 module Steps
   module Submission
     class MoreInformationForm < Steps::BaseFormObject
+      attribute :additional_information_required, :value_object, source: YesNoAnswer
       attribute :additional_information, :string
 
+      validates :additional_information_required, inclusion: { in: :choices }
+
       validates :additional_information, presence: true,
-        unless: -> { more_information_not_needed? }
+                if: -> { additional_information_needed? }
 
       def persist!
-        self.additional_information = nil if more_information_not_needed?
-
-        crime_application.update(additional_information:)
+        crime_application.update(attributes)
       end
 
       def choices
         YesNoAnswer.values
       end
 
-      def need_more_information
-        return @need_more_information unless @need_more_information.nil?
-
-        YesNoAnswer::YES if crime_application.additional_information.present?
-      end
-
-      def need_more_information=(attr)
-        @need_more_information = YesNoAnswer.new(attr)
-      end
-
       private
 
-      def more_information_not_needed?
-        need_more_information == YesNoAnswer::NO
+      def additional_information_needed?
+        additional_information_required == YesNoAnswer::YES
+      end
+
+      def before_save
+        self.additional_information = nil unless additional_information_needed?
       end
     end
   end
