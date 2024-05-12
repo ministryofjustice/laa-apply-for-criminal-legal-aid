a5c257a448430b93a03b815a527dc0295516a48f
Ripan <er.ripankumar@gmail.com>
2024-05-13 00:05:46 +0100

    create employers WIP

---
+++ app/forms/steps/income/employer_details_form.rb
@@ -0,0 +1,16 @@
+module Steps
+  module Income
+    class EmployerDetailsForm < Steps::BaseFormObject
+
+      attribute :employer_name
+      # attribute :address
+      # attribute :address_line_one
+      # attribute :address_line_two
+      # attribute :city
+      # attribute :country
+      # attribute :postcode
+      #
+      # validates :address, presence: true
+    end
+  end
+end
