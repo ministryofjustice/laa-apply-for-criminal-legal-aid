116e7930b60e086b06f8e49038aae9193d56acfc
Ripan <er.ripankumar@gmail.com>
2024-05-13 13:54:39 +0100

    Support ownership type

--- db/migrate/20240512222640_create_employments.rb
+++ db/migrate/20240512222640_create_employments.rb
@@ -2,6 +2,7 @@ class CreateEmployments < ActiveRecord::Migration[7.0]
   def change
     create_table :employments, id: :uuid do |t|
       t.references :crime_application, type: :uuid, foreign_key: true, null: false
+      t.string :ownership_type, null: false, default: "applicant"
       t.string :employer_name
       t.jsonb :address
 
