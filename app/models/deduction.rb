319a313a8664b1f0cef1345ef7d16e2f66f18f50
Ripan <er.ripankumar@gmail.com>
2024-05-17 13:51:17 +0100

    Add associations

---
+++ app/models/deduction.rb
@@ -0,0 +1,7 @@
+class Deduction < ApplicationRecord
+  belongs_to :employment
+  validates :amount, numericality: { greater_than: 0 }
+  validates :frequency, presence: true, inclusion: { in: PaymentFrequencyType.values.map(&:to_s) }
+  validates :deduction_type, presence: true, inclusion: { in: DeductionType.values.map(&:to_s) }
+  validates :employment_id, uniqueness: { scope: :deduction_type }
+end
--- app/models/employment.rb
+++ app/models/employment.rb
@@ -1,6 +1,8 @@
 class Employment < ApplicationRecord
   belongs_to :crime_application
 
+  has_many :deductions, dependent: :destroy
+
   store_accessor :address, :address_line_one, :address_line_two, :city, :country, :postcode
   store_accessor :metadata, [:before_or_after_tax]
 end
