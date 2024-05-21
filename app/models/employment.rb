a18e46b0b94c32c01ac14c3e6bad9e65f4a3854d
Ripan <er.ripankumar@gmail.com>
2024-05-21 17:36:18 +0100

    Fix errors

--- app/models/employment.rb
+++ app/models/employment.rb
@@ -1,8 +1,6 @@
 class Employment < ApplicationRecord
-  store_accessor :metadata,
-                 [:before_or_after_tax]
-
   belongs_to :crime_application
 
   store_accessor :address, :address_line_one, :address_line_two, :city, :country, :postcode
+  store_accessor :metadata, [:before_or_after_tax]
 end
