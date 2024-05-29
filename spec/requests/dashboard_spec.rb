c5d34dbaa8e43035db4a966fd5cfc09eeda2730e
Arthur Ashman <45827968+arthurashman@users.noreply.github.com>
2024-05-29 13:57:30 +0300

    Switch on non means tested (#870)
    Switch on no means tested feature flag, and show page, Move has partner page if passported partner ff enabled, If has partner page before tasklist, non means q shows before that, If has partner page in new position (after has nino) non means is first question

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -10,33 +10,82 @@ RSpec.describe 'Dashboard', :authorized do
   let(:returned_application_fixture_id) { '47a93336-7da6-48ec-b139-808ddd555a41' }
 
   describe 'start a new application' do
-    context 'when non_means_tested feature flag is enabled' do
-      before do
-        allow(FeatureFlags).to receive(:non_means_tested) {
-          instance_double(FeatureFlags::EnabledFeature, enabled?: true)
-        }
+    # before do
+    #   allow(FeatureFlags).to receive(:non_means_tested) {
+    #     instance_double(FeatureFlags::EnabledFeature, enabled?: non_means_tested_enabled)
+    #   }
+    #   allow(FeatureFlags).to receive(:passported_partner_journey) {
+    #                            instance_double(FeatureFlags::EnabledFeature,
+    #                                            enabled?: passported_partner_journey_enabled)
+    #                          }
+    # end
+    before do
+      allow(FeatureFlags).to receive_messages(
+        non_means_tested: instance_double(FeatureFlags::EnabledFeature, enabled?: non_means_tested_enabled),
+        passported_partner_journey: instance_double(FeatureFlags::EnabledFeature,
+                                                    enabled?: passported_partner_journey_enabled)
+      )
+    end
+
+    let(:non_means_tested_enabled) { nil }
+
+    context 'when passported partner journey feature flag is not enabled' do
+      let(:passported_partner_journey_enabled) { false }
+
+      context 'when non_means_tested feature flag is enabled' do
+        let(:non_means_tested_enabled) { true }
+
+        it 'creates a new `crime_application` record' do
+          expect { post crime_applications_path }.to change(CrimeApplication, :count).by(1)
+
+          expect(response).to have_http_status(:redirect)
+          expect(response).to redirect_to(/is_application_means_tested/)
+        end
       end
 
-      it 'creates a new `crime_application` record' do
-        expect { post crime_applications_path }.to change(CrimeApplication, :count).by(1)
+      context 'when `non means tested` feature flag is not enabled' do
+        before do
+          allow(FeatureFlags).to receive(:non_means_tested) {
+            instance_double(FeatureFlags::EnabledFeature, enabled?: false)
+          }
+        end
 
-        expect(response).to have_http_status(:redirect)
-        expect(response).to redirect_to(/is_application_means_tested/)
+        it 'creates a new `crime_application` record' do
+          expect { post crime_applications_path }.to change(CrimeApplication, :count).by(1)
+
+          expect(response).to have_http_status(:redirect)
+          expect(response).to redirect_to(/has_partner/)
+        end
       end
     end
 
-    context 'when `non means tested` feature flag is not enabled' do
-      before do
-        allow(FeatureFlags).to receive(:non_means_tested) {
-          instance_double(FeatureFlags::EnabledFeature, enabled?: false)
-        }
+    context 'when passported partner journey feature flag is enabled' do
+      let(:passported_partner_journey_enabled) { true }
+
+      context 'when non_means_tested feature flag is enabled' do
+        let(:non_means_tested_enabled) { true }
+
+        it 'creates a new `crime_application` record' do
+          expect { post crime_applications_path }.to change(CrimeApplication, :count).by(1)
+
+          expect(response).to have_http_status(:redirect)
+          expect(response).to redirect_to(/applications/)
+        end
       end
 
-      it 'creates a new `crime_application` record' do
-        expect { post crime_applications_path }.to change(CrimeApplication, :count).by(1)
+      context 'when `non means tested` feature flag is not enabled' do
+        before do
+          allow(FeatureFlags).to receive(:non_means_tested) {
+            instance_double(FeatureFlags::EnabledFeature, enabled?: false)
+          }
+        end
 
-        expect(response).to have_http_status(:redirect)
-        expect(response).to redirect_to(/has_partner/)
+        it 'creates a new `crime_application` record' do
+          expect { post crime_applications_path }.to change(CrimeApplication, :count).by(1)
+
+          expect(response).to have_http_status(:redirect)
+          expect(response).to redirect_to(/applications/)
+        end
       end
     end
   end
