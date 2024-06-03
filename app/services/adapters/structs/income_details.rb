98b5694ca3c5aea784aa9ef41c158837e472cdd6
Ripan <er.ripankumar@gmail.com>
2024-06-03 18:25:58 +0100

    Fix rehydration

--- app/services/adapters/structs/income_details.rb
+++ app/services/adapters/structs/income_details.rb
@@ -10,13 +10,25 @@ module Adapters
         partner_employment_type || []
       end
 
+      def employments
+        return [] unless __getobj__
+
+        super.map do |attrs|
+          Employment.new(**attrs)
+        end
+      end
+
       def serializable_hash(options = {})
         super(
           options.merge(
             methods: [:employment_status, :partner_employment_status],
             # `employment_type` is the name for employment_status
             # in the datastore, we don't use it
-            except: [:employment_type, :partner_employment_type, :dependants, :income_payments, :income_benefits]
+            except: [
+              :employment_type, :partner_employment_type,
+              :dependants, :income_payments, :income_benefits,
+              :employments
+            ]
           )
         )
       end
