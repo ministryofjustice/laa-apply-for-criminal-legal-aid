ea9df340d0ff477532a697304a82eef27a7764fa
Ripan <er.ripankumar@gmail.com>
2024-06-07 14:17:18 +0100

    Fix review page display

--- app/presenters/summary/sections/self_assessment_tax_bill.rb
+++ app/presenters/summary/sections/self_assessment_tax_bill.rb
@@ -4,12 +4,14 @@ module Summary
       def show?
         crime_application.outgoings.applicant_self_assessment_tax_bill.present?
       end
+
       def answers
-        answers = []
-        answers << Components::ValueAnswer.new(
-          :self_assessment_tax_bill, crime_application.outgoings.applicant_self_assessment_tax_bill,
-          change_path: edit_steps_income_client_self_assessment_tax_bill_path
-        )
+        answers = [
+          Components::ValueAnswer.new(
+            :self_assessment_tax_bill, crime_application.outgoings.applicant_self_assessment_tax_bill,
+            change_path: edit_steps_income_client_self_assessment_tax_bill_path
+          )
+        ]
 
         if crime_application.outgoings.applicant_self_assessment_tax_bill == YesNoAnswer::YES.to_s
           answers << Components::PaymentAnswer.new(
@@ -22,7 +24,7 @@ module Summary
       end
 
       def self_assessment_tax_bill
-        crime_application.outgoings_payments.self_assessment_tax_bill
+        crime_application.outgoings_payments.detect { |payment| payment.payment_type == OutgoingsPaymentType::SELF_ASSESSMENT_TAX_BILL.to_s }
       end
     end
   end
