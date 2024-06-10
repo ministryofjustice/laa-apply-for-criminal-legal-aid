3f01658ac5d1696c2d359fe2ca4a22ce67a7f6cb
Hibo <51047911+hiboabd@users.noreply.github.com>
2024-06-10 11:10:47 +0100

    [CRIMAPP-896] Add partner to benefit check task list validation (#914)
    Add partner to benefit check task list validation

--- spec/validators/passporting_benefit_check/answers_validator_spec.rb
+++ spec/validators/passporting_benefit_check/answers_validator_spec.rb
@@ -3,13 +3,15 @@ require 'rails_helper'
 RSpec.describe PassportingBenefitCheck::AnswersValidator, type: :model do
   subject(:validator) { described_class.new(record) }
 
-  let(:record) { instance_double(CrimeApplication, errors:, applicant:) }
+  let(:record) { instance_double(CrimeApplication, errors:, applicant:, partner:, partner_detail:) }
   let(:errors) { double(:errors, empty?: false) }
   let(:applicant) { instance_double(Applicant, benefit_type:) }
   let(:benefit_type) { nil }
   let(:appeal_no_changes?) { false }
   let(:under18?) { false }
   let(:means_passported?) { false }
+  let(:partner) { nil }
+  let(:partner_detail) { nil }
 
   before do
     allow(record).to receive_messages(appeal_no_changes?: appeal_no_changes?)
@@ -75,6 +77,17 @@ RSpec.describe PassportingBenefitCheck::AnswersValidator, type: :model do
 
         it { is_expected.to be(true) }
       end
+    end
+
+    context 'when the partner is the benefit check recipient' do
+      let(:partner_detail) { double(PartnerDetail, involvement_in_case: 'none') }
+      let(:benefit_type) { BenefitType::NONE.to_s }
+      let(:partner) {
+        double(Partner, id: '234', benefit_type: BenefitType::UNIVERSAL_CREDIT.to_s,
+                             nino: 'AB123456A', has_benefit_evidence: 'yes')
+      }
+
+      it { is_expected.to be(true) }
     end
   end
 
