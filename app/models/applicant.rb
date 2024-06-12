f6b545875d3ea08b94b05d1a0d3b3619db7a3bc6
timpeat <tim@timpeat.com>
2024-06-12 21:37:34 +0100

    Update rule 22 for partner

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,4 +1,22 @@
 class Applicant < Person
+  has_many(
+    :income_payments,
+    -> { where(ownership_type: OwnershipType::APPLICANT.to_s) },
+    through: :crime_application
+  )
+
+  has_many(
+    :income_benefits,
+    -> { where(ownership_type: OwnershipType::APPLICANT.to_s) },
+    through: :crime_application
+  )
+
+  has_many(
+    :national_savings_certificates,
+    -> { where(ownership_type: OwnershipType::APPLICANT.to_s) },
+    through: :crime_application
+  )
+
   # Utility methods for testing/output
   delegate :partner_detail, to: :crime_application
 
--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,2 +1,19 @@
 class Partner < Person
+  has_many(
+    :income_payments,
+    -> { where(ownership_type: OwnershipType::PARTNER.to_s) },
+    through: :crime_application
+  )
+
+  has_many(
+    :income_benefits,
+    -> { where(ownership_type: OwnershipType::PARTNER.to_s) },
+    through: :crime_application
+  )
+
+  has_many(
+    :national_savings_certificates,
+    -> { where(ownership_type: OwnershipType::PARTNER.to_s) },
+    through: :crime_application
+  )
 end
