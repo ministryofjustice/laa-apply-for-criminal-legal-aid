4c30552866da4f72809eb8e1f0f83ad8c5413785
RoseSAK <rose.azadkan@digital.justice.gov.uk>
2024-06-13 19:51:11 +0100

    CRIMAPP-876 add validation for employed client journey

--- app/models/concerns/type_of_employment.rb
+++ app/models/concerns/type_of_employment.rb
@@ -7,6 +7,10 @@ module TypeOfEmployment
     income.employment_status.include?(EmploymentStatus::NOT_WORKING.to_s)
   end
 
+  def employed?
+    income.employment_status.include?(EmploymentStatus::EMPLOYED.to_s)
+  end
+
   def ended_employment_within_three_months?
     income.ended_employment_within_three_months == 'yes'
   end
