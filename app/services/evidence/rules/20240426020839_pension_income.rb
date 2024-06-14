5338b5ae63c3300bbb460b483d12c7eeab6d8bf7
timpeat <tim@timpeat.com>
2024-06-14 16:32:23 +0100

    CRIMAPP-991 partner pension evidence rule

--- app/services/evidence/rules/20240426020839_pension_income.rb
+++ app/services/evidence/rules/20240426020839_pension_income.rb
@@ -4,25 +4,28 @@ module Evidence
       include Evidence::RuleDsl
 
       # Note value in pounds, db pence/amounts are stored in pennies
+      # Note this is per month
       THRESHOLD = 1000.00
 
       key :income_private_pension_5
       group :income
 
-      client do |crime_application|
-        income = [
-          crime_application.income_payments.private_pension,
-        ]
-
-        total = income.compact.sum { |x| x.prorated_monthly.to_f }
-
-        total > THRESHOLD
+      client do |_crime_application, applicant|
+        PensionEvidenceRequired.for(applicant)
       end
 
-      # TODO: Awaiting partner implementation
-      partner do |_crime_application|
-        false
+      partner do |_crime_application, partner|
+        PensionEvidenceRequired.for(partner)
       end
+    end
+  end
+
+  class PensionEvidenceRequired
+    def self.for(person)
+      pension = person.income_payments.private_pension
+      return false unless pension
+
+      pension.prorated_monthly.to_f > Rules::PrivatePensionIncome::THRESHOLD
     end
   end
 end
