5bce701c8716e69bcdb990b4109240362e752de9
Ripan <er.ripankumar@gmail.com>
2024-06-17 18:52:32 +0100

    Update deduction_fieldset_form.rb

--- app/forms/steps/income/partner/deduction_fieldset_form.rb
+++ app/forms/steps/income/partner/deduction_fieldset_form.rb
@@ -5,16 +5,6 @@ module Steps
         def employment
           crime_application.partner_employments.find(employment_id)
         end
-
-        private
-
-        def details_only_when_other?
-          if (deduction_type == DeductionType::OTHER.to_s) && details.blank?
-            errors.add(:details, :blank)
-          elsif (deduction_type != DeductionType::OTHER.to_s) && details.present?
-            errors.add(:details, :invalid)
-          end
-        end
       end
     end
   end
