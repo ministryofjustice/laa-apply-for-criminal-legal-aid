fcfd970fd830d083b3de67433061aa4d1475c3b1
Arthur Ashman <arthur.ashman@digital.justice.gov.uk>
2024-06-26 17:44:08 +0100

    Add declaration details to completed applicaiton view

--- app/presenters/summary/sections/declarations.rb
+++ app/presenters/summary/sections/declarations.rb
@@ -7,24 +7,33 @@ module Summary
         true
       end
 
-      # rubocop:disable Metrics/MethodLength
+      # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
       def answers
-        [
-          Components::ValueAnswer.new(
-            :legal_rep_has_client_declaration,
-            'yes'
-          ),
-          Components::ValueAnswer.new(
-            :legal_rep_has_partner_declaration,
-            provider_details.legal_rep_has_partner_declaration,
-          ),
-          Components::FreeTextAnswer.new(
-            :legal_rep_no_partner_declaration_reason,
-            provider_details.legal_rep_no_partner_declaration_reason,
-          ),
-        ]
+        answers =
+          [
+            Components::ValueAnswer.new(
+              :legal_rep_has_client_declaration,
+              'yes'
+            )
+          ]
+        if provider_details.legal_rep_has_partner_declaration.present?
+          answers.push(Components::ValueAnswer.new(
+                         :legal_rep_has_partner_declaration,
+                         provider_details.legal_rep_has_partner_declaration,
+                       ))
+        end
+
+        if provider_details.legal_rep_has_partner_declaration == 'no' &&
+           provider_details.legal_rep_no_partner_declaration_reason
+          answers.push(Components::FreeTextAnswer.new(
+                         :legal_rep_no_partner_declaration_reason,
+                         provider_details.legal_rep_no_partner_declaration_reason,
+                       ))
+        end
+
+        answers.flatten.select(&:show?)
       end
-      # rubocop:enable Metrics/MethodLength
+      # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
 
       private
 
