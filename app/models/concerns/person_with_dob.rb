940101f1b0a9173773c0cdec0184b45957de06b4
Ripan <er.ripankumar@gmail.com>
2024-07-02 11:22:56 +0100

    Update person_with_dob.rb

--- app/models/concerns/person_with_dob.rb
+++ app/models/concerns/person_with_dob.rb
@@ -1,11 +1,9 @@
 module PersonWithDob
   extend ActiveSupport::Concern
 
-  # :nocov:
   def under18?
     raise Errors::DateOfBirthPending, 'date of birth is not set' if date_of_birth.blank?
 
     date_of_birth + 18.years > Time.zone.today
   end
-  # :nocov:
 end
