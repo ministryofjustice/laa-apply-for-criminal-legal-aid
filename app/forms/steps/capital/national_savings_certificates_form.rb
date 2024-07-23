df0585048643d89040c8d579807b47d616d9f2f3
Rose <rose.azadkhan@digital.justice.gov.uk>
2024-07-23 14:01:50 +0100

    CRIMAPP-1235 Add validation for national savings certificate format (#1069)
    prevent entering strings that end with - to avoid modsec errors
    
    Co-authored-by: RoseSAK <rose.azadkan@digital.justice.gov.uk>

--- app/forms/steps/capital/national_savings_certificates_form.rb
+++ app/forms/steps/capital/national_savings_certificates_form.rb
@@ -5,13 +5,18 @@ module Steps
       include OwnershipConfirmation
       include ApplicantOrPartner
 
+      # match any alphanumeric string that does not contain any special characters
+      # this is to avoid modsec errors, specifically strings ending with - (dash)
+      NSC_REGEXP = /\A[\w\s]+\z/
+
       delegate :national_savings_certificate_type, to: :record
 
       attribute :holder_number, :string
       attribute :certificate_number, :string
       attribute :value, :pence
 
-      validates :holder_number, :certificate_number, :value, presence: true
+      validates :holder_number, format: { with: NSC_REGEXP }, presence: true
+      validates :certificate_number, :value, presence: true
 
       def persist!
         record.update(attributes)
