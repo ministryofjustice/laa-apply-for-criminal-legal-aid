c386c12553e6663b9e8436ad5c79301612b4af38
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2024-07-29 09:53:21 +0100

    Update percentage validation

--- app/forms/steps/capital/property_form.rb
+++ app/forms/steps/capital/property_form.rb
@@ -27,7 +27,7 @@ module Steps
 
       validates_numericality_of :percentage_applicant_owned, greater_than_or_equal_to: 0.0,
                                 less_than_or_equal_to: 100.0, if: :include_partner_in_means_assessment?
-      validates_numericality_of :percentage_partner_owned, greater_than: 0.0,
+      validates_numericality_of :percentage_partner_owned, greater_than_or_equal_to: 0.0,
                                 less_than_or_equal_to: 100.0, if: :include_partner_in_means_assessment?
 
       validates_with CapitalAssessment::PropertyOwnershipValidator
