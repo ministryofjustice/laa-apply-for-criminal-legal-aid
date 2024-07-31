528400cc4100c1b2ea8623c9bf3ac909fae20358
timpeat <tim@timpeat.com>
2024-07-31 16:33:49 +0100

    LAA-CRIMINAL-APPLICATIONS-DATASTORE-6
    Refactor MeansPassporter to be stateless.

--- app/services/passporting/means_passporter.rb
+++ app/services/passporting/means_passporter.rb
@@ -2,19 +2,23 @@ module Passporting
   class MeansPassporter < BasePassporter
     include TypeOfMeansAssessment
 
-    def call
-      crime_application.update(means_passport:)
+    def call(persist: false)
+      crime_application.update(means_passport:) unless persist
 
       passported?
     end
 
     def means_passport
+      return @means_passport if @means_passport
+
       passport = []
       passport << MeansPassportType::ON_NOT_MEANS_TESTED if app_not_means_tested?
       passport << MeansPassportType::ON_AGE_UNDER18      if age_passported?
       passport << MeansPassportType::ON_BENEFIT_CHECK    if benefit_check_passed?
-      passport
+
+      @means_passport = passport
     end
+    alias passport_types_collection means_passport
 
     def passported?
       not_means_tested? || age_passported? || benefit_check_passported?
@@ -24,28 +28,27 @@ module Passporting
       app_not_means_tested?
     end
 
-    # Age based passporting persists on resubmission
     def age_passported?
-      return passported_on?(MeansPassportType::ON_AGE_UNDER18) if resubmission?
+      parent_was_age_passported? || applicant_under18?
+    end
 
-      applicant_under18?
+    def parent_was_age_passported?
+      return false unless resubmission?
+      return false if crime_application.means_passport.blank?
+
+      crime_application.means_passport.include?(MeansPassportType::ON_AGE_UNDER18.to_s)
     end
 
     def benefit_check_passported?
       benefit_check_passed?
     end
 
-    def passport_types_collection
-      crime_application.means_passport
-    end
-
     private
 
     def app_not_means_tested?
       return false unless FeatureFlags.non_means_tested.enabled?
-      return true if crime_application.is_means_tested == 'no'
 
-      false
+      crime_application.is_means_tested == 'no'
     end
 
     def applicant_under18?
