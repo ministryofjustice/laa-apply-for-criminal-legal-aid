8a80c761f1cd28cad6ceb817bca9d5dda20becca
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2024-08-13 10:52:34 +0100

    WIP updates to summary cards and review application page

--- app/presenters/summary/sections/overview.rb
+++ app/presenters/summary/sections/overview.rb
@@ -9,6 +9,9 @@ module Summary
       def answers
         relevant_answers =
           [
+            Components::ValueAnswer.new(
+              :application_type, crime_application.application_type
+            ),
             Components::FreeTextAnswer.new(
               :reference, crime_application.reference.to_s,
             ),
