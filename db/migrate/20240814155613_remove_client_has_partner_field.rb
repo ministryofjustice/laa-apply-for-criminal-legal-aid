0bec157c1b37230b3ac4ffe30f572043ff11f3cc
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2024-08-15 09:19:21 +0100

    CRIMAPP-936 Update partner column default

--- db/migrate/20240814155613_remove_client_has_partner_field.rb
+++ db/migrate/20240814155613_remove_client_has_partner_field.rb
@@ -1,10 +1,12 @@
 class RemoveClientHasPartnerField < ActiveRecord::Migration[7.0]
   def up
     remove_column :crime_applications, :client_has_partner
+    change_column_default :partner_details, :has_partner, nil
   end
 
   def down
     add_column :crime_applications, :client_has_partner, :string
+    change_column_default :partner_details, :has_partner, 'no'
 
     PartnerDetail.all.pluck(:crime_application_id, :has_partner).each do |id, value|
       CrimeApplication.find_by(id:)&.update(client_has_partner: value)
