67fe026b6a986d558a0c45cdbdcc9fd518a96fe7
timpeat <tim@timpeat.com>
2024-08-19 10:00:37 +0100

    [CRIMAPP-1310] Allow self assessment tax bill to be zero (#1129)
    Allow self assessment tax bill to be zero

--- app/forms/steps/income/partner/self_assessment_tax_bill_form.rb
+++ app/forms/steps/income/partner/self_assessment_tax_bill_form.rb
@@ -11,7 +11,7 @@ module Steps
 
         validates :partner_self_assessment_tax_bill, inclusion: { in: YesNoAnswer.values }
         validates :partner_self_assessment_tax_bill_amount,
-                  numericality: { greater_than: 0 }, if: -> { pays_self_assessment_tax_bill? }
+                  numericality: { greater_than_or_equal_to: 0 }, if: -> { pays_self_assessment_tax_bill? }
         validates :partner_self_assessment_tax_bill_frequency,
                   inclusion: { in: PaymentFrequencyType.values }, if: -> { pays_self_assessment_tax_bill? }
 
