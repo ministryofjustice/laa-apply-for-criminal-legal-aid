5a7bfbd27f377163dc42c800907153cbcd3e491b
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2024-08-22 09:59:25 +0100

    CRIMAPP-1257 Update decision tree for cifc

--- app/services/decisions/client_decision_tree.rb
+++ app/services/decisions/client_decision_tree.rb
@@ -38,7 +38,7 @@ module Decisions
     private
 
     def after_client_details
-      if FeatureFlags.non_means_tested.enabled?
+      if FeatureFlags.non_means_tested.enabled? && !crime_application.cifc?
         edit(:is_means_tested)
       else
         edit(:case_type)
