f48df5acd38e8703ec5acd72ecbde4c0c52dccdb
EdwinKruglov <edwin.kruglov@digital.justice.gov.uk>
2024-09-02 09:58:18 +0100

    [CRIMAPP-1314] Update contact page content

--- spec/system/contact_us_page_spec.rb
+++ spec/system/contact_us_page_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'Contact us page' do
   it 'shows the contact us page with the correct contact details' do
     expect(page).to have_css('h1', text: 'Contact us')
 
-    expect(page).to have_css('p', text: 'Telephone: 0300 200 2020')
-    expect(page).to have_css('p', text: 'Email: apply-for-criminal-legal-aid@justice.gov.uk')
+    expect(page).to have_css('p', text: '0300 200 2020')
+    expect(page).to have_css('p', text: 'online-support@justice.gov.uk')
   end
 end
