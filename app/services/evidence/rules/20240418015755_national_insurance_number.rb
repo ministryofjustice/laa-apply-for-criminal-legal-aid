d66dff27c5dc57621ce51be358727c181c0f1862
Mo Seedat <47113046+mseedat-moj@users.noreply.github.com>
2024-09-17 09:38:38 +0100

    [CRIMAPP-1311] Update evidence rule for national insurance rule 32 (#1173)
    CRIMAPP-1311 Update evidence rule for national insurance

--- app/services/evidence/rules/20240418015755_national_insurance_number.rb
+++ app/services/evidence/rules/20240418015755_national_insurance_number.rb
@@ -9,15 +9,17 @@ module Evidence
       client do |crime_application|
         status = crime_application.applicant&.benefit_check_status
         income_benefits = crime_application.income&.client_income_benefits || []
+        has_nino = crime_application.applicant&.nino.present?
 
-        income_benefits.any? || (status.present? && BenefitCheckStatus.new(status).no_check_no_nino?)
+        !has_nino && (income_benefits.any? || (status.present? && BenefitCheckStatus.new(status).no_check_no_nino?))
       end
 
       partner do |crime_application|
         status = crime_application.partner&.benefit_check_status
         income_benefits = crime_application.income&.partner_income_benefits || []
+        has_nino = crime_application.partner&.nino.present?
 
-        income_benefits.any? || (status.present? && BenefitCheckStatus.new(status).no_check_no_nino?)
+        !has_nino && (income_benefits.any? || (status.present? && BenefitCheckStatus.new(status).no_check_no_nino?))
       end
     end
   end
