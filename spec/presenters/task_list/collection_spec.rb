32e6f57a23f5a6ef853765c5f24cb6c1c6cb04bc
timpeat <tim@timpeat.com>
2025-01-06 10:25:25 +0000

    [CRIMAPP-1553] replace MoJ task list with Govuk one (#1299)
    [CRIMAPP-1553] replace MoJ task list with Govuk

--- spec/presenters/task_list/collection_spec.rb
+++ spec/presenters/task_list/collection_spec.rb
@@ -1,58 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe TaskList::Collection do
-  include ActionView::TestCase::Behavior
-
-  subject { described_class.new(view, crime_application:) }
+  subject(:collection) { described_class.new(crime_application:) }
 
   let(:name) { :foobar_task }
   let(:crime_application) { double application_type: 'initial' }
 
-  describe 'collection of sections' do
+  describe '#sections' do
+    subject(:sections) { collection.sections }
+
     it 'has the `client_details` section' do
-      expect(subject[0].index).to eq(1)
-      expect(subject[0].name).to eq(:about_your_client)
-      expect(subject[0].tasks).to eq([:client_details, :partner_details, :passporting_benefit_check])
+      expect(sections[0].name).to eq(:about_your_client)
+      expect(sections[0].tasks.map(&:name)).to eq([:client_details, :partner_details, :passporting_benefit_check])
     end
 
     it 'has the `case_details` section' do
-      expect(subject[1].index).to eq(2)
-      expect(subject[1].name).to eq(:case_details)
-      expect(subject[1].tasks).to eq([:case_details, :ioj])
+      expect(sections[1].name).to eq(:case_details)
+      expect(sections[1].tasks.map(&:name)).to eq([:case_details, :ioj])
     end
 
     it 'has the `means_assessment` section' do
-      expect(subject[2].index).to eq(3)
-      expect(subject[2].name).to eq(:means_assessment)
-      expect(subject[2].tasks).to eq([:income_assessment, :outgoings_assessment, :capital_assessment])
+      expect(sections[2].name).to eq(:means_assessment)
+      expect(sections[2].tasks.map(&:name)).to eq([:income_assessment, :outgoings_assessment, :capital_assessment])
     end
 
     it 'has the `support_evidence` section' do
-      expect(subject[3].index).to eq(4)
-      expect(subject[3].name).to eq(:support_evidence)
-      expect(subject[3].tasks).to eq([:evidence_upload])
+      expect(sections[3].name).to eq(:support_evidence)
+      expect(sections[3].tasks.map(&:name)).to eq([:evidence_upload])
     end
 
     it 'has the `more_information` section' do
-      expect(subject[4].index).to eq(5)
-      expect(subject[4].name).to eq(:more_information)
-      expect(subject[4].tasks).to eq([:more_information])
+      expect(sections[4].name).to eq(:more_information)
+      expect(sections[4].tasks.map(&:name)).to eq([:more_information])
     end
 
     it 'has the `review_confirm` section' do
-      expect(subject[5].index).to eq(6)
-      expect(subject[5].name).to eq(:review_confirm)
-      expect(subject[5].tasks).to eq([:review, :declaration])
+      expect(sections[5].name).to eq(:review_confirm)
+      expect(sections[5].tasks.map(&:name)).to eq([:review, :declaration])
     end
   end
 
   describe '#completed' do
     before do
-      allow(TaskList::Task).to receive(:new).and_return(task_completed, task_incomplete)
+      allow(Tasks::BaseTask).to receive(:build).and_return(task_completed, task_incomplete)
     end
 
-    let(:task_completed) { double(completed?: true) }
-    let(:task_incomplete) { double(completed?: false) }
+    let(:task_completed) { double(status: double(completed?: true)) }
+    let(:task_incomplete) { double(status: double(completed?: false)) }
 
     it 'returns only the completed tasks' do
       expect(subject.completed).to eq([task_completed])
@@ -60,33 +54,16 @@ RSpec.describe TaskList::Collection do
   end
 
   describe '#applicable' do
-    let(:applicable_task) { double(not_applicable?: false) }
+    let(:applicable_task) { double(status: double(not_applicable?: false)) }
 
     before do
-      allow(TaskList::Task).to receive(:new).and_return(
-        applicable_task, double(not_applicable?: true)
+      allow(Tasks::BaseTask).to receive(:build).and_return(
+        applicable_task, double(status: double(not_applicable?: true))
       )
     end
 
     it 'returns only the applicable tasks' do
       expect(subject.applicable).to eq([applicable_task])
-    end
-  end
-
-  describe '#render' do
-    before do
-      # We test the Section separately, here we don't need to
-      allow_any_instance_of(TaskList::Section).to receive(:render).and_return('[section_markup]')
-    end
-
-    it 'iterates through the sections defined, rendering each one' do
-      expect(
-        TaskList::Section
-      ).to receive(:new).exactly(6).times.and_return(double.as_null_object)
-
-      expect(
-        subject.render
-      ).to match(%r{<ol class="moj-task-list">.*</ol>})
     end
   end
 end
