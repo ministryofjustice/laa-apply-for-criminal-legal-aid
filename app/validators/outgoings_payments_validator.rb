5a907e2af7e3d7987b6554a7d039ed209ce6a0c5
Hibo <51047911+hiboabd@users.noreply.github.com>
2025-01-14 15:47:10 +0000

    [CRIMAPP-1403] Update payment fieldset form validation (#1307)
    * Update payment fieldset form validation
    
    * Fix failing specs
    
    * Fix linting error
    
    * Fix failing spec
    
    * Refactor
    
    * Code coverage fix

--- app/validators/outgoings_payments_validator.rb
+++ app/validators/outgoings_payments_validator.rb
@@ -1,55 +1,5 @@
-class OutgoingsPaymentsValidator < ActiveModel::Validator
-  attr_reader :record
-
-  def validate(record)
-    @record = record
-
-    record.types.each_with_index do |type, index|
-      next if type == 'none'
-
-      outgoings_payment = record.public_send(type)
-      add_indexed_errors(outgoings_payment, index) unless outgoings_payment.valid?
-    end
-
-    return unless record.types.empty?
-
-    record.errors.add(:base, :none_selected) if record.has_no_other_outgoings.blank?
-  end
-
-  private
-
-  def add_indexed_errors(outgoings_payment, index)
-    outgoings_payment.errors.each do |error|
-      attr_name = indexed_attribute(index, outgoings_payment, error.attribute)
-
-      record.errors.add(
-        attr_name,
-        error.type,
-        message: error_message(outgoings_payment, error)
-      )
-
-      # We define the attribute getter as it doesn't really exist
-      record.define_singleton_method(attr_name) do
-        outgoings_payment.public_send(error.attribute)
-      end
-    end
-  end
-
-  def indexed_attribute(_index, outgoings_payment, attr)
-    "#{outgoings_payment.payment_type.dasherize}-#{attr}"
-  end
-
-  # `activemodel.errors.models.steps/outgoings/outgoing_payment_fieldset_form.summary.x.y`
-  def error_message(obj, error)
-    payment_type = I18n.t(
-      obj.payment_type,
-      scope: [:helpers, :label, :steps_outgoings_outgoings_payments_form, :types_options]
-    )
-
-    I18n.t(
-      "#{obj.model_name.i18n_key}.summary.#{error.attribute}.#{error.type}",
-      scope: [:activemodel, :errors, :models],
-      payment_type: payment_type
-    )
+class OutgoingsPaymentsValidator < BasePaymentsValidator
+  def has_no_payments?
+    record.has_no_other_outgoings.blank?
   end
 end
--- app/validators/partner_income_benefits_validator.rb
+++ app/validators/partner_income_benefits_validator.rb
@@ -1,56 +1,5 @@
-class PartnerIncomeBenefitsValidator < ActiveModel::Validator
-  attr_reader :record
-
-  def validate(record)
-    @record = record
-
-    record.types.each_with_index do |type, index|
-      next if type == 'none'
-
-      income_benefit = record.public_send(type)
-      add_indexed_errors(income_benefit, index) unless income_benefit.valid?
-    end
-
-    return unless record.types.empty?
-
-    record.errors.add(:base, :none_selected) if record.partner_has_no_income_benefits.blank?
-  end
-
-  private
-
-  def add_indexed_errors(income_benefit, index)
-    income_benefit.errors.each do |error|
-      attr_name = indexed_attribute(index, income_benefit, error.attribute)
-
-      record.errors.add(
-        attr_name,
-        error.type,
-        message: error_message(income_benefit, error)
-      )
-
-      # We define the attribute getter as it doesn't really exist
-      record.define_singleton_method(attr_name) do
-        income_benefit.public_send(error.attribute)
-      end
-    end
-  end
-
-  def indexed_attribute(_index, income_benefit, attr)
-    "#{income_benefit.payment_type.dasherize}-#{attr}"
-  end
-
-  # `activemodel.errors.models.steps/income/partner/income_benefit_fieldset_form.summary.x.y`
-  def error_message(obj, error)
-    payment_type = I18n.t(
-      obj.payment_type,
-      scope: [:helpers, :label, :steps_income_income_benefits_form, :types_options]
-    )
-    payment_type&.downcase! if obj.payment_type == IncomeBenefitType::OTHER.to_s
-
-    I18n.t(
-      "#{obj.model_name.i18n_key}.summary.#{error.attribute}.#{error.type}",
-      scope: [:activemodel, :errors, :models],
-      payment_type: payment_type
-    )
+class PartnerIncomeBenefitsValidator < BasePaymentsValidator
+  def has_no_payments?
+    record.partner_has_no_income_benefits.blank?
   end
 end
