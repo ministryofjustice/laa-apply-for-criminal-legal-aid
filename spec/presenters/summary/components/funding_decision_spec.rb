0fee9dc1467619021094b28bc0dd919e9905228b
timpeat <tim@timpeat.com>
2025-01-20 16:13:57 +0000

    CRIMAPP-1581 prepare for outcome status user research (#1315)
    CRIMAPP-1581 show simplified overall result
    
    - remove colour tags from overall result
    - show court_type (Means test)

--- spec/presenters/summary/components/funding_decision_spec.rb
+++ spec/presenters/summary/components/funding_decision_spec.rb
@@ -8,17 +8,16 @@ describe Summary::Components::FundingDecision, type: :component do
 
   let(:attributes) do
     {
-      case_id:,
-      maat_id:,
-      interests_of_justice:,
-      means:,
-      funding_decision:,
-      comment:
+      case_id: 'APP123',
+      maat_id: 'M123',
+      interests_of_justice: interests_of_justice,
+      means: means,
+      funding_decision: funding_decision,
+      court_type: 'crown',
+      comment: 'Decision comment'
     }
   end
 
-  let(:case_id) { 'APP123' }
-  let(:maat_id) { 'M123' }
   let(:interests_of_justice) do
     instance_double(
       LaaCrimeSchemas::Structs::TestResult,
@@ -28,17 +27,19 @@ describe Summary::Components::FundingDecision, type: :component do
       assessed_on: Date.new(2024, 10, 8),
     )
   end
+
   let(:means) do
     instance_double(
       LaaCrimeSchemas::Structs::TestResult,
-      result: 'failed',
+      result: means_result,
       details: 'Means details',
       assessed_by: 'Grace Nolan',
       assessed_on: Date.new(2024, 10, 9),
     )
   end
+
   let(:funding_decision) { 'refused' }
-  let(:comment) { 'Decision comment' }
+  let(:means_result) { 'failed' }
 
   before { component }
 
@@ -47,6 +48,7 @@ describe Summary::Components::FundingDecision, type: :component do
       expect(summary_card('Case')).to have_rows(
         'MAAT ID', 'M123',
         'Case number', 'APP123',
+        'Means test', 'Crown Court',
         'Interests of justice (IoJ) test result', 'Failed',
         'IoJ comment', 'IoJ details',
         'IoJ caseworker', 'Grace Nolan',
@@ -58,5 +60,25 @@ describe Summary::Components::FundingDecision, type: :component do
         'Further information about the decision', 'Decision comment'
       )
     end
+  end
+
+  context 'when funding decision is "granted" and means "passed"' do
+    let(:funding_decision) { 'granted' }
+    let(:means_result) { 'passed' }
+
+    it { is_expected.to have_text('Granted') }
+  end
+
+  context 'when funding decision is "granted" and means "passed_with_contribution"' do
+    let(:funding_decision) { 'granted' }
+    let(:means_result) { 'passed_with_contribution' }
+
+    it { is_expected.to have_text('Granted - with a contribution') }
+  end
+
+  context 'when funding decision is "granted" and means "failed"' do
+    let(:funding_decision) { 'granted' }
+
+    it { is_expected.to have_text('Granted - failed means test') }
   end
 end
