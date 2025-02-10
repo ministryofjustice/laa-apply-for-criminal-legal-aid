98400875d9525f57adad5f787074a9b24a74bfab
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2025-02-10 11:48:16 +0000

    validate other owner

--- app/validators/capital_assessment/property_ownership_validator.rb
+++ app/validators/capital_assessment/property_ownership_validator.rb
@@ -5,14 +5,30 @@ module CapitalAssessment
     def validate(form)
       @form = form
 
-      form.errors.add :percentage_applicant_owned, error_key(form) unless valid_ownership_total?(form)
-
+      validate_applicant_ownership(form) unless other_owners?(form)
+      validate_other_owners(form) if other_owners?(form)
       return unless form.include_partner_in_means_assessment? && !valid_ownership_total?(form)
 
-      form.errors.add :percentage_partner_owned, error_key(form)
+      validate_partner_ownership(form) unless other_owners?(form)
     end
 
     private
+
+    def validate_applicant_ownership(form)
+      return if valid_ownership_total?(form)
+
+      form.errors.add :percentage_applicant_owned, error_key(form)
+    end
+
+    def validate_partner_ownership(form)
+      form.errors.add :percentage_partner_owned, error_key(form)
+    end
+
+    def validate_other_owners(form)
+      return if valid_ownership_total?(form)
+
+      form.errors.add :has_other_owners, error_key(form)
+    end
 
     def valid_ownership_total?(form)
       percentage_ownerships = all_percentage_ownerships(form)
