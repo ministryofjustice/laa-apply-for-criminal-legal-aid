9d50818e970fba0649c2ad23ddd0da20d5740df1
timpeat <tim@timpeat.com>
2025-02-27 18:10:21 +0000

    [CRIMAPP-1405] ignore age passport on resubmission if dob changes

--- spec/system/applying/passported_on_age_spec.rb
+++ spec/system/applying/passported_on_age_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe 'Apply for Criminal Legal Aid when age passported' do
     end
   end
 
-  context 'when date_stampable and applicant under 18 when drafted' do
+  context 'when date_stampable and applicant under 18 when date_stamped' do
     before do
       draft_age_passported_application(case_type: 'Either way')
     end
@@ -54,7 +54,55 @@ RSpec.describe 'Apply for Criminal Legal Aid when age passported' do
 
       it 'previously passported sections remain passported' do
         click_link('Review the application')
+        expect(page).not_to have_content I18n.t('errors.incomplete_records')
+      end
+    end
+  end
+
+  describe 'resubmitting a previously age passported application when now over 18' do
+    before do
+      draft_age_passported_application(case_type:)
+      click_button 'Save and submit application'
+
+      click_link('Sign out')
+      travel 1.week
+
+      visit root_path
+      click_button('Start now')
+      choose('Yes')
+      return_submitted_application
+
+      click_button('Update application')
+    end
+
+    context 'when Indictable' do
+      let(:case_type) { 'Indictable' }
+
+      it 'the resubmission is not age passported' do
+        expect(page).to have_content I18n.t('errors.incomplete_records')
+      end
+    end
+
+    context 'when Either way' do
+      let(:case_type) { 'Either way' }
+
+      it 'the resubmission is age passported' do
+        expect(page).not_to have_content I18n.t('errors.incomplete_records')
+      end
+    end
+
+    context 'when the Date of Birth changed so origional would not have been passported' do
+      let(:case_type) { 'Either way' }
+
+      before do
+        within_card('Client details') do |card|
+          first('a', text: 'Change').click
+        end
+        fill_in('Year', with: 2000)
         save_and_continue
+        save_and_continue
+        click_button('Save and come back later')
+        expect(page).to have_text 'You have completed 0 of 9 sections'
       end
     end
   end
