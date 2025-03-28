551aeacd7b94c92f0b92fb2c2972c31150a6d814
timpeat <tim@timpeat.com>
2025-03-28 15:15:56 +0000

    Do not pre-compile in production (#1385)
    * Do not pre-compile in production
    
    * Fix incorrect content type on error pages
    
    Ensure error responses use an explicit content type when not HTML.
    Previously, a missing JS file would trigger a 404 response with a
    Content-Type of application/javascript, which violated CSP policies.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,12 +24,8 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
-  # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :sass
-
-  # We are pre-compiling assets, so on production these will always be present, however
-  # for this to work this flag needs to be `true`. Seems like a Rails bug to me.
-  config.assets.compile = true
+  # Cache assets for far-future expiry since they are all digest stamped.
+  config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
