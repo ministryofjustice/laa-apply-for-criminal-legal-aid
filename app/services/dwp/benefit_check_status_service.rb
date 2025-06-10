d0dcabb7bdfd22da4b5613403d7b653e3be3def7
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2025-06-10 10:14:13 +0100

    WIP display undtermined status

--- app/services/dwp/benefit_check_status_service.rb
+++ app/services/dwp/benefit_check_status_service.rb
@@ -22,24 +22,28 @@ module DWP
     attr_reader :crime_application, :person
 
     def benefit_check_status
-      return BenefitCheckStatus::NO_CHECK_NO_NINO.to_s if nino_forthcoming?
-      return undetermined_status if dwp_undetermined
       return BenefitCheckStatus::NO_CHECK_REQUIRED.to_s if benefit_check_subject.benefit_type == 'none'
+      return BenefitCheckStatus::NO_CHECK_NO_NINO.to_s if nino_forthcoming?
       return BenefitCheckStatus::CHECKER_UNAVAILABLE.to_s if checker_down
+      return BenefitCheckStatus::UNDETERMINED.to_s if undetermined
+      return BenefitCheckStatus::NO_RECORD_FOUND.to_s if not_in_receipt
 
-      BenefitCheckStatus::CONFIRMED.to_s if benefit_check_subject.benefit_check_result
+      BenefitCheckStatus::CONFIRMED.to_s if confirmed
     end
 
-    def undetermined_status
-      return BenefitCheckStatus::UNDETERMINED.to_s if benefit_evidence_forthcoming?
-
-      BenefitCheckStatus::NO_RECORD_FOUND.to_s if means_assessment_as_benefit_evidence?
+    def undetermined
+      benefit_check_subject.dwp_response == 'Undetermined' || (benefit_check_subject.confirm_dwp_result == 'no' && benefit_evidence_forthcoming?)
     end
 
-    def dwp_undetermined
-      benefit_check_subject.confirm_dwp_result == 'no'
+    def not_in_receipt
+      benefit_check_subject.dwp_response == 'No' || (benefit_check_subject.confirm_dwp_result == 'no' && means_assessment_as_benefit_evidence?)
     end
 
+    def confirmed
+      benefit_check_subject.dwp_response == 'Yes' || benefit_check_subject.benefit_check_result
+    end
+
+    # TODO: does this logic hold up??
     def checker_down
       benefit_check_subject.benefit_check_result.nil? && benefit_check_subject.has_benefit_evidence.present?
     end
