45a7f6a52dce953b2c0a3ef58f82a29478aa05fa
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2025-06-30 14:59:11 +0100

    language toggle using the url params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,8 @@ class ApplicationController < ActionController::Base
 
   add_flash_types :success
 
+  around_action :switch_locale
+
   def current_crime_application
     @current_crime_application ||= CrimeApplication.find_by(
       id: application_id,
@@ -20,6 +22,15 @@ class ApplicationController < ActionController::Base
     @current_office_code ||= current_provider&.selected_office_code
   end
   helper_method :current_office_code
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options(options = {})
+    { locale: I18n.locale }.merge(options)
+  end
 
   private
 
