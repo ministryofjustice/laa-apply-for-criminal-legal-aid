4e3593c8bf74f65ec95aa4d316e171f23c314ba1
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2025-07-01 13:43:15 +0100

    more translations

--- config/initializers/govuk_components.rb
+++ config/initializers/govuk_components.rb
@@ -1,4 +1,4 @@
 Govuk::Components.configure do |conf|
-  conf.default_header_service_name = 'Apply for criminal legal aid'
+  conf.default_header_service_name = I18n.t('layouts.header.service_name')
   conf.default_header_homepage_url = 'https://www.gov.uk'
 end
