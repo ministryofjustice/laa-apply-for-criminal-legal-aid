57823c01e27ea001242f3d9dde196b516f71d563
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2025-07-30 09:22:14 +0100

    test for enabled flag

--- spec/system/language_toggle_spec.rb
+++ spec/system/language_toggle_spec.rb
@@ -17,4 +17,20 @@ RSpec.describe 'Language toggle' do
       expect(page).not_to have_css('.govuk-link--no-underline', text: 'English')
     end
   end
+
+  context 'when welsh translation feature flag is enabled' do
+    before do
+      allow(FeatureFlags).to receive(:welsh_translation) {
+        instance_double(FeatureFlags::EnabledFeature, enabled?: true)
+      }
+
+      visit root_path
+    end
+
+    it 'does not show the language toggle' do
+      expect(page).to have_css('.language-toggle')
+      expect(page).to have_link('Cymraeg', href: '/applications?locale=cy')
+      expect(page).to have_css('.govuk-link--no-underline', text: 'English')
+    end
+  end
 end
