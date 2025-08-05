2f84c079e7bbd0a95cc33bba3074a04665f33362
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2025-08-05 16:00:12 +0100

    rubocop

--- app/services/providers/office_router.rb
+++ app/services/providers/office_router.rb
@@ -1,7 +1,7 @@
 module Providers
   class OfficeRouter
-    include Rails.application.routes.url_helpers,
-            Routing
+    include Rails.application.routes.url_helpers
+    include Routing
 
     attr_reader :provider
 
