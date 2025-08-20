6c27ccd4a0efdfed334079d10f52cf6d38466a02
EdwinKruglov <edwin.kruglov@justice.gov.uk>
2025-08-20 10:56:06 +0100

    Remove redundant code

--- app/models/crime_application.rb
+++ app/models/crime_application.rb
@@ -148,8 +148,4 @@ class CrimeApplication < ApplicationRecord # rubocop:disable Metrics/ClassLength
   def review_status
     nil
   end
-
-  def soft_deleted?
-    soft_deleted_at.present?
-  end
 end
