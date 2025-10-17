e1e0df9fe353aa880e1d5b9e9ad7c1d7cacb6d27
EdwinKruglov <edwin.kruglov@justice.gov.uk>
2025-10-17 15:55:20 +0100

    Address feedback

--- app/services/datastore/events/draft_created.rb
+++ app/services/datastore/events/draft_created.rb
@@ -3,7 +3,7 @@ module Datastore
     class DraftCreated
       attr_reader :entity_id, :entity_type, :business_reference, :created_at
 
-      def initialize(entity_id:, entity_type:, business_reference:, created_at: nil)
+      def initialize(entity_id:, entity_type:, business_reference:, created_at: Time.zone.now)
         @entity_id = entity_id
         @entity_type = entity_type
         @business_reference = business_reference
