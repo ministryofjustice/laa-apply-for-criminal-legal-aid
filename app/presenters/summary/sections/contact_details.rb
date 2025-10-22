b010fde353f09f147572f078d2bbe02be18739fb
hiboabd <51047911+hiboabd@users.noreply.github.com>
2025-10-22 16:43:31 +0100

    Add welsh correspondence feature flag

--- app/presenters/summary/sections/contact_details.rb
+++ app/presenters/summary/sections/contact_details.rb
@@ -46,7 +46,8 @@ module Summary
           ),
 
           Components::FreeTextAnswer.new(
-            :requested_welsh_correspondence, requested_welsh_correspondence, show: true,
+            :requested_welsh_correspondence, requested_welsh_correspondence,
+            show: FeatureFlags.correspondence_preference.enabled?,
             change_path: edit_steps_client_contact_details_path
           ),
         ]
