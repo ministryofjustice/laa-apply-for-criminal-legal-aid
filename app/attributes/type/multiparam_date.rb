1c3873ef8d76936133820bfb3d74351ea510ab95
Edwin Kruglov <edwin.kruglov@justice.gov.uk>
2025-11-27 09:18:00 +0000

    [CRIMAPP-1733] Allow months names in date input (#1594)

--- app/attributes/type/multiparam_date.rb
+++ app/attributes/type/multiparam_date.rb
@@ -8,9 +8,9 @@ module Type
     def cast(value)
       return value if value.blank? || value.is_a?(Date)
 
-      # `value` is a hash in the format: {3=>31, 2=>12, 1=>2000}
-      # where `3` is the day, `2` is the month and `1` is the year.
-      value_args = value.values_at(1, 2, 3)
+      # `value` is a hash in the format: {3=>31, 2=>'12', 1=>2000}
+      # where `3` is the day, `2` is the month (digit or month name) and `1` is the year.
+      value_args = normalize_month_of_date(value).values_at(1, 2, 3)
 
       if Date.valid_date?(*value_args)
         Date.new(*value_args)
@@ -27,6 +27,23 @@ module Type
     # Leave the sanity check to the `#cast` method.
     def value_from_multiparameter_assignment(value)
       value
+    end
+
+    private
+
+    # Attempt to parse the month, which could be a digit or a month name
+    # Date::parse is quite tolerant of abbreviations of variable length and slight misspellings
+    # Assumes value[2] to be the month value
+    def normalize_month_of_date(value)
+      normalized = value.dup
+      month_value = value[2]
+
+      begin
+        normalized[2] = month_value.to_i.nonzero? || Date.parse(month_value).month
+        normalized
+      rescue StandardError
+        value
+      end
     end
   end
 end
--- app/helpers/form_builder_helper.rb
+++ app/helpers/form_builder_helper.rb
@@ -10,13 +10,23 @@ module FormBuilderHelper
     end
   end
 
-  def segment_names
-    { day: I18n.t('date.day'), month: I18n.t('date.month'), year: I18n.t('date.year') }
+  def date_input(attribute_name, opts = {}, &block)
+    govuk_date_field(attribute_name, segments:, segment_names:, **opts, &block)
   end
 
   private
 
   def submit_button(i18n_key, opts = {}, &block)
     govuk_submit I18n.t("helpers.submit.#{i18n_key}"), **opts, &block
+  end
+
+  def segment_names
+    { day: I18n.t('date.day'), month: I18n.t('date.month'), year: I18n.t('date.year') }
+  end
+
+  # This ensures that the month segment is not automatically cast to integer by the form class,
+  # allowing month names to be entered.
+  def segments
+    { day: '3i', month: '2', year: '1i' }
   end
 end
