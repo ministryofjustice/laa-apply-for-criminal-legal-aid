f6d55eb47c11ee4617a6c8f511dbc445e6262c8a
timpeat <tim@timpeat.com>
2026-02-09 17:37:51 +0000

    Parallelize CI tests across 8 nodes
    - Split test job into lint + test (8-node matrix) + coverage merge
    - Sort examples by ID before slicing for deterministic splitting
      (rspec --dry-run output is non-deterministic between runs)
    - SimpleCov configured with track_files, enable_coverage_for_eval,
      and unique command_name per node
    - Coverage merge rake task collates results and enforces 100% minimum
    - Build job now depends on lint + test + coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,33 @@
 ENV['RAILS_ENV'] ||= 'test'
 
-require 'simplecov'
+unless ENV['COVERAGE'] == 'false'
+  require 'simplecov'
 
-SimpleCov.minimum_coverage 100
+  SimpleCov.start 'rails' do
+    minimum_coverage 100 unless ENV['CI_NODE_INDEX']
 
-SimpleCov.start 'rails' do
-  add_filter 'app/mailers/application_mailer.rb'
-  add_filter 'app/jobs/application_job.rb'
-  add_filter 'config/initializers'
-  add_filter 'config/routes.rb'
-  add_filter 'lib/rubocop/'
-  add_filter 'spec/'
+    add_filter 'app/mailers/application_mailer.rb'
+    add_filter 'app/jobs/application_job.rb'
+    add_filter 'config/initializers'
+    add_filter 'config/routes.rb'
+    add_filter 'lib/rubocop/'
+    add_filter 'spec/'
+
+    # Track all application files to ensure consistent coverage across parallel runs
+    track_files '{app,lib}/**/*.rb'
+
+    # Support for parallel CI runs - each runner saves results with unique ID
+    command_name "Job #{ENV['GITHUB_JOB']}-#{ENV['CI_NODE_INDEX']}" if ENV['GITHUB_JOB'] && ENV['CI_NODE_INDEX']
+
+    if ENV['CI']
+      formatter SimpleCov::Formatter::SimpleFormatter
+    else
+      formatter SimpleCov::Formatter::MultiFormatter.new([
+                                                           SimpleCov::Formatter::SimpleFormatter,
+                                                           SimpleCov::Formatter::HTMLFormatter
+                                                         ])
+    end
+  end
 end
 
 RSpec.configure do |config|
