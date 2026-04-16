5d3c70481cd4a13f3bf4d4be459b4430f2fc379e
timpeat <tim@timpeat.com>
2026-04-16 11:34:31 +0100

    Add frame and form actions to CSP

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -14,15 +14,18 @@ Rails.application.configure do
     policy.font_src    :self
 
     if FeatureFlags.google_analytics.enabled?
-      policy.connect_src :self, 'https://ga.jspm.io', 'https://*.google-analytics.com', 'https://*.analytics.google.com', 'https://*.googletagmanager.com'
+      policy.connect_src :self, 'https://ga.jspm.io', 'https://*.google-analytics.com',
+                         'https://*.analytics.google.com', 'https://*.googletagmanager.com'
       policy.img_src :self, 'https://*.google-analytics.com', 'https://*.googletagmanager.com'
     else
       policy.img_src :self
     end
 
-    policy.object_src  :none
-    policy.script_src  :self
-    policy.style_src   :self
+    policy.object_src      :none
+    policy.script_src      :self
+    policy.style_src       :self
+    policy.frame_ancestors :none
+    policy.form_action     :self
 
     # Specify URI for violation reports
     # policy.report_uri "/csp-violation-report-endpoint"
@@ -30,7 +33,7 @@ Rails.application.configure do
 
   # Generate session nonces for permitted importmap and inline scripts
   config.content_security_policy_nonce_generator = ->(_request) { SecureRandom.base64(16) }
-  config.content_security_policy_nonce_directives = %w(script-src style-src)
+  config.content_security_policy_nonce_directives = %w[script-src style-src]
 
   # Report violations without enforcing the policy.
   # config.content_security_policy_report_only = true
