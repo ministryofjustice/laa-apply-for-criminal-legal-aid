98d917c4c2ecf0bb26a191baf41b11aa54b8eee4
timpeat <tim@timpeat.com>
2026-04-20 10:27:01 +0100

    Add entra to form_action CSP

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
     policy.script_src      :self
     policy.style_src       :self
     policy.frame_ancestors :none
-    policy.form_action     :self
+    policy.form_action     :self, 'https://login.microsoftonline.com'
 
     # Specify URI for violation reports
     # policy.report_uri "/csp-violation-report-endpoint"
