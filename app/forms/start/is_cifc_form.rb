9248a8e3d137a42d7f1a6a90245754c9802dc1ef
Mooktakim Ahmed <mooktakim.ahmed@justice.gov.uk>
2026-09-04 17:40:01 +0100

    CRIMAPP-2352 Localise new application/CIFC selection error message

--- app/forms/start/is_cifc_form.rb
+++ app/forms/start/is_cifc_form.rb
@@ -2,10 +2,7 @@ module Start
   class IsCifcForm < Steps::BaseFormObject
     attribute :is_cifc, :value_object, source: YesNoAnswer
 
-    validates :is_cifc, inclusion: {
-      in: :choices,
-      message: I18n.t("#{to_s.underscore}.is_cifc.inclusion", scope: [:activemodel, :errors, :models])
-    }
+    validates :is_cifc, inclusion: { in: :choices }
 
     def self.build(crime_application, is_cifc)
       form = new(crime_application:)
